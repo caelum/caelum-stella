083cd0826e8ba3a1ee6b82ca77c3e1802d675205
Fabio Kung <fabio.kung@gmail.com>
2008-03-04 22:45:53 +0000

    teste de integracao para o validador faces de cpf

--- stella-faces/src/test/java/br/com/caelum/stella/faces/validation/StellaCPFValidatorTest.java
+++ stella-faces/src/test/java/br/com/caelum/stella/faces/validation/StellaCPFValidatorTest.java
@@ -5,11 +5,17 @@ import org.jmock.Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import javax.faces.application.Application;
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIViewRoot;
 import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import java.util.ResourceBundle;
+import java.util.Locale;
 
 /**
  * StellaCPFValidator integration tests
@@ -30,27 +36,67 @@ public class StellaCPFValidatorTest {
 
     @Test
     public void shouldNotThrowValidatorExceptionForValidCPF() throws Exception {
-        final ResourceBundle bundle = new SimpleMapBundle();
         final FacesContext context = mockery.mock(FacesContext.class);
         final UIComponent component = mockery.mock(UIComponent.class);
+        mockFacesContext(context, "messages", Locale.getDefault());
+
+        validator.validate(context, component, "75885366160");
+        mockery.assertIsSatisfied();
+    }
+
+    @Test
+    public void shouldGiveMessagesFromBrazilianResourceBundleForInvalidCpfAndPtBTLocale() throws Exception {
+        final FacesContext context = mockery.mock(FacesContext.class);
+        final UIComponent component = mockery.mock(UIComponent.class);
+        mockFacesContext(context, "messages", new Locale("pt", "BR"));
+
+        try {
+            validator.validate(context, component, "1234567765");
+            fail();
+        } catch (ValidatorException e) {
+            // it should throw exception for invalid cpf
+            FacesMessage message = e.getFacesMessage();
+            assertEquals("CPF Invalido", message.getSummary());
+            mockery.assertIsSatisfied();
+        }
+    }
+
+
+    @Test
+    public void shouldGiveMessagesFromDefaultResourceBundleForInvalidCpfAndEnUSLocale() throws Exception {
+        final FacesContext context = mockery.mock(FacesContext.class);
+        final UIComponent component = mockery.mock(UIComponent.class);
+        mockFacesContext(context, "messages", new Locale("en"));
+
+        try {
+            validator.validate(context, component, "1234567765");
+            fail();
+        } catch (ValidatorException e) {
+            // it should throw exception for invalid cpf
+            FacesMessage message = e.getFacesMessage();
+            assertEquals("Invalid CPF", message.getSummary());
+            mockery.assertIsSatisfied();
+        }
+    }
+
+    private void mockFacesContext(final FacesContext context, final String bundleName, final Locale locale) {
         mockery.checking(new Expectations() {
             {
-                String bundleName = "any";
                 Application application = mockery.mock(Application.class);
-
                 allowing(context).getApplication();
                 will(returnValue(application));
 
-//                allowing(application).getMessageBundle();
-//                will(returnValue(bundleName));
-//
-//                allowing(application).getResourceBundle(context, bundleName);
-//                will(returnValue(bundle));
+                allowing(application).getMessageBundle();
+                will(returnValue(bundleName));
+
+                UIViewRoot viewRoot = mockery.mock(UIViewRoot.class);
+                allowing(context).getViewRoot();
+                will(returnValue(viewRoot));
+
+                allowing(viewRoot).getLocale();
+                will(returnValue(locale));
             }
         });
-
-//        validator.validate(context, component, "75885366160");
-        mockery.assertIsSatisfied();
     }
 
 }
