d1c842827c2642d94f3336bbbdae930e86415fd3
Leonardo Bessa <leobessa@gmail.com>
2008-03-11 13:28:37 +0000

    Adicionando NIT no UsuarioBean

--- examples/faces-example/src/main/java/br/com/caelum/stella/faces/example/UsuarioBean.java
+++ examples/faces-example/src/main/java/br/com/caelum/stella/faces/example/UsuarioBean.java
@@ -4,48 +4,54 @@ package br.com.caelum.stella.faces.example;
  * Managed bean que recebe o cpf do formulario
  *
  * @author Fabio Kung
+ * @author Leonardo Bessa
  */
 public class UsuarioBean {
 
     private String cpf;
     private String cpfFormatado;
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        System.out.println("Mudando o cpf para: " + cpf);
-        this.cpf = cpf;
-    }
-
-    public String getCpfFormatado() {
-        return cpfFormatado;
-    }
-
-    public void setCpfFormatado(String cpfFormatado) {
-        System.out.println("Mudando o cpf formtado para: " + cpfFormatado);
-        this.cpfFormatado = cpfFormatado;
-    }
     
     private String cnpj;
     private String cnpjFormatado;
+    
+    private String nit;
+    private String nitFormatado;
+    
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCpfFormatado() {
+		return cpfFormatado;
+	}
+	public void setCpfFormatado(String cpfFormatado) {
+		this.cpfFormatado = cpfFormatado;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getCnpjFormatado() {
+		return cnpjFormatado;
+	}
+	public void setCnpjFormatado(String cnpjFormatado) {
+		this.cnpjFormatado = cnpjFormatado;
+	}
+	public String getNit() {
+		return nit;
+	}
+	public void setNit(String nit) {
+		this.nit = nit;
+	}
+	public String getNitFormatado() {
+		return nitFormatado;
+	}
+	public void setNitFormatado(String nitFormatado) {
+		this.nitFormatado = nitFormatado;
+	}
 
-    public String getcnpj() {
-        return cnpj;
-    }
-
-    public void setcnpj(String cnpj) {
-        System.out.println("Mudando o cnpj para: " + cnpj);
-        this.cnpj = cnpj;
-    }
-
-    public String getcnpjFormatado() {
-        return cnpjFormatado;
-    }
-
-    public void setcnpjFormatado(String cnpjFormatado) {
-        System.out.println("Mudando o cnpj formtado para: " + cnpjFormatado);
-        this.cnpjFormatado = cnpjFormatado;
-    }
 }
