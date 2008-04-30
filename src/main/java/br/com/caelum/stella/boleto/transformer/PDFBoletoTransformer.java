b6c2d3669757e0cd8959bcb0d03d33c13cd98372
Paulo Silveira <paulo.silveira@caelum.com.br>
2008-04-30 20:56:35 +0000

    reorganizando banco para interface, limpando JBoleto.

---
+++ src/main/java/br/com/caelum/stella/boleto/transformer/PDFBoletoTransformer.java
@@ -0,0 +1,26 @@
+package br.com.caelum.stella.boleto.transformer;
+
+import java.io.InputStream;
+
+import br.com.caelum.stella.boleto.Boleto;
+
+/**
+ * Dada as informações de um boleto, gera um PDF desde.
+ * 
+ * 
+ * @author Paulo Silveira
+ *
+ */
+public class PDFBoletoTransformer implements BoletoTransformer {
+
+	/**
+	 * Gera o PDF em memoria e aloca-o em um InputStream.
+	 * 
+	 */
+	public InputStream transform(Boleto boleto) {
+		
+		
+		return null;
+	}
+
+}
