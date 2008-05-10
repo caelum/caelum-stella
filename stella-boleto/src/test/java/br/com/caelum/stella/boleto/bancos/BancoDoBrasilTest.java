c95d1430b0a5f670ecd791d12e59deb39edbf838
Paulo Silveira <paulo.silveira@caelum.com.br>
2008-05-10 16:41:44 +0000

    uma classe de teste pra cada banco

--- stella-boleto/src/test/java/br/com/caelum/stella/boleto/bancos/BancoDoBrasilTest.java
+++ stella-boleto/src/test/java/br/com/caelum/stella/boleto/bancos/BancoDoBrasilTest.java
@@ -1,4 +1,4 @@
-package br.com.caelum.stella.boleto;
+package br.com.caelum.stella.boleto.bancos;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -6,14 +6,17 @@ import static org.junit.Assert.assertNotNull;
 import java.io.IOException;
 import java.util.Calendar;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import br.com.caelum.stella.boleto.bancos.BancoDoBrasil;
-import br.com.caelum.stella.boleto.bancos.Bradesco;
+import br.com.caelum.stella.boleto.Banco;
+import br.com.caelum.stella.boleto.BoletoPadrao;
+import br.com.caelum.stella.boleto.DatasPadrao;
+import br.com.caelum.stella.boleto.EmissorPadrao;
+import br.com.caelum.stella.boleto.SacadoPadrao;
 
-public class BancosTest {
+public class BancoDoBrasilTest {
+
 	private BoletoPadrao boleto;
 	private Banco banco;
 
@@ -22,7 +25,7 @@ public class BancosTest {
 		DatasPadrao datas = DatasPadrao.newDatas().comDocumento(
 				dataPara(4, 5, 2008)).comProcessamento(dataPara(4, 5, 2008))
 				.comVencimento(dataPara(2, 5, 2008));
-		
+
 		EmissorPadrao emissor = EmissorPadrao.newEmissor().comCedente("Caue")
 				.comAgencia("1824").comDvAgencia("4").comContaCorrente(
 						"0076000").comNumConvenio("1207113").comDvContaCorrete(
@@ -32,16 +35,16 @@ public class BancosTest {
 
 		banco = new BancoDoBrasil();
 
-		boleto = BoletoPadrao.newBoleto().comDatas(datas)
-				.comEmissor(emissor).comSacado(sacado).comValorBoleto("40.00")
-				.comNoDocumento("4323");
+		boleto = BoletoPadrao.newBoleto().comDatas(datas).comEmissor(emissor)
+				.comSacado(sacado).comValorBoleto("40.00").comNoDocumento(
+						"4323");
 	}
 
 	@Test
 	public void testLinhaDoBancoDoBrasil() {
 		banco = new BancoDoBrasil();
 		boleto = boleto.comBanco(banco);
-		
+
 		assertEquals(
 				"00190.00009  01207.113000  09000.206186  5  38600000004000",
 				banco.geraLinhaDigitavelPara(boleto));
@@ -53,25 +56,6 @@ public class BancosTest {
 		boleto = boleto.comBanco(banco);
 
 		assertEquals("00195386000000040000000001207113000900020618", banco
-				.geraCodigoDeBarrasPara(boleto));
-	}
-	
-	@Test
-	public void testLinhaDoBradesco() {
-		banco = new Bradesco();
-		boleto = boleto.comBanco(banco);
-		
-		assertEquals(
-				"23790.18241  18000.900029  06007.600007  5  38600000007000",
-				banco.geraLinhaDigitavelPara(boleto));
-	}
-
-	@Test
-	public void testCodigoDeBarraDoBradesco() {
-		banco = new Bradesco();
-		boleto = boleto.comBanco(banco);
-		
-		assertEquals("23795386000000070000182418000900020600760000", banco
 				.geraCodigoDeBarrasPara(boleto));
 	}
 
