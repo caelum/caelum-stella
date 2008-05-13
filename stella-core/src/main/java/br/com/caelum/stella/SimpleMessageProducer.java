a6835c63260913e90b3cb8785ecdb0a930788b94
Leonardo Bessa <leobessa@gmail.com>
2008-05-13 16:13:46 +0000

    DOCUMETANCAO DO STELLA CORE / EXEMPLO DO STELLA CORE COM RESCOURCE_BUNLDE / ADICAO DO SimpleMessageProducer no CS CORE / MINOR na documentacao do CS FACES

---
+++ stella-core/src/main/java/br/com/caelum/stella/SimpleMessageProducer.java
@@ -0,0 +1,47 @@
+package br.com.caelum.stella;
+
+import br.com.caelum.stella.validation.InvalidValue;
+
+/**
+ * <p>
+ * SimpleMessageProducer é responsável pela geração de mensagens de erro. Estas
+ * mensagens são geradas atraves dos nomes das anotoções que representam os
+ * erros.
+ * </p>
+ * <p>
+ * A messagem de erro é composta do seguinte modo:
+ * </p>
+ * <code>
+ String simpleName = error.getClass().getSimpleName();
+ String errorName = error.name();
+ String key = (simpleName + "." + errorName);
+ String message;
+ message = key.replaceFirst("[.]", " : ").replaceAll("_", " ");
+ *</code>
+ * </p>
+ * <p>
+ * Veja o exemplo:
+ * </p>
+ * <p>
+ * A mesagem do erro representado por CPFError.INVALID_DIGITS é
+ * cpferror : invalid digits .
+ * </p>
+ * 
+ * @author Leonardo Bessa
+ * 
+ */
+@SuppressWarnings("unchecked")
+public class SimpleMessageProducer implements MessageProducer {
+
+	/**
+	 * @see br.com.caelum.stella.MessageProducer#getMessage(java.lang.Enum)
+	 */
+	public ValidationMessage getMessage(InvalidValue error) {
+		String simpleName = error.getClass().getSimpleName();
+		String errorName = error.name();
+		String key = (simpleName + "." + errorName);
+		String message;
+		message = key.replaceFirst("[.]", " : ").replaceAll("_", " ");
+		return new SimpleValidationMessage(message);
+	}
+}
---
+++ stella-core/src/test/java/br/com/caelum/stella/SimpleMessageProducerTest.java
@@ -0,0 +1,23 @@
+package br.com.caelum.stella;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.stella.validation.InvalidValue;
+
+public class SimpleMessageProducerTest {
+
+	private enum DocumentoError implements InvalidValue{
+		INVALID_DIGITS,INVALID_CHECK_DIGITS;
+	}
+	
+	@Test
+	public void testGetMessage() {
+		SimpleMessageProducer messageProducer = new SimpleMessageProducer();
+		ValidationMessage message = messageProducer.getMessage(DocumentoError.INVALID_CHECK_DIGITS);
+		assertEquals("DocumentoError : INVALID CHECK DIGITS", message.getMessage());
+	}
+
+}
