ca108db07231c59045bda5be1f5fd27f1e8fda90
Caue Guerra <caueguerra@gmail.com>
2008-05-13 03:51:36 +0000

    boleto
    corrigindo tamanho imagem

--- stella-boleto/src/main/java/br/com/caelum/stella/boleto/transformer/PNGBoletoWriter.java
+++ stella-boleto/src/main/java/br/com/caelum/stella/boleto/transformer/PNGBoletoWriter.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.Transparency;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
