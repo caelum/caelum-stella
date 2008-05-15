879d6f75d2e3c171a75c3586859c0cb880a741e3
Paulo Silveira <paulo.silveira@caelum.com.br>
2008-05-15 21:48:25 +0000

    removendo Validator duplicado e javadoc.

--- stella-core/src/main/java/br/com/caelum/stella/validation/Validator.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/Validator.java
@@ -6,21 +6,28 @@ import br.com.caelum.stella.ValidationMessage;
 
 /**
  * <p>
- * {@link #invalidMessagesFor(Object))} devolve os problemas de
- * validação associado ao objeto.
+ * Interface que os validadores do Stella devem implementar.
+ * 
+ * {@link #invalidMessagesFor(Object))} devolve os problemas de validação
+ * associado ao objeto.
+ * 
+ * {@link #assertValid(Object))} valida o objeto e lança uma exception no caso
+ * inválido.
+ * 
  * </p>
  * 
  * @author Fabio Kung
  * @author Leonardo Bessa
  * @param <T>
- *       tipo de objeto a ser validado
+ *            tipo de objeto a ser validado
  */
 public interface Validator<T> {
 	/**
 	 * @param object
 	 *            objeto que se desejar validar
 	 * @throws {@link InvalidStateException}
-	 *             se o parametro não for válido
+	 *             se o parametro não for válido, contendo a lista de
+	 *             ValidationMessage
 	 */
 	void assertValid(T object);
 
