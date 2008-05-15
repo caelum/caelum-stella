8b014e8599b5360359b2afe5581af74721346be5
Paulo Silveira <paulo.silveira@caelum.com.br>
2008-05-15 21:26:21 +0000

    minor javadocs

--- stella-core/src/main/java/br/com/caelum/stella/validation/error/CNPJError.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/error/CNPJError.java
@@ -3,9 +3,12 @@ package br.com.caelum.stella.validation.error;
 import br.com.caelum.stella.validation.InvalidValue;
 
 /**
+ * 
+ * Representa possíveis erros do CNPJ
+ * 
  * @author Leonardo Bessa
  * @author Fabio Kung
  */
 public enum CNPJError implements InvalidValue {
-    INVALID_CHECK_DIGITS, INVALID_DIGITS, INVALID_FORMAT
+	INVALID_CHECK_DIGITS, INVALID_DIGITS, INVALID_FORMAT
 }
--- stella-core/src/main/java/br/com/caelum/stella/validation/error/IEError.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/error/IEError.java
@@ -3,8 +3,11 @@ package br.com.caelum.stella.validation.error;
 import br.com.caelum.stella.validation.InvalidValue;
 
 /**
+ * 
+ * Representa possíveis erros durante validação de uma inscrição estadual
+ * 
  * @author Leonardo Bessa
  */
-public enum IEError implements InvalidValue{
-    INVALID_CHECK_DIGITS, INVALID_DIGITS, INVALID_FORMAT, INVALID_MUNICIPALITY
+public enum IEError implements InvalidValue {
+	INVALID_CHECK_DIGITS, INVALID_DIGITS, INVALID_FORMAT, INVALID_MUNICIPALITY
 }
