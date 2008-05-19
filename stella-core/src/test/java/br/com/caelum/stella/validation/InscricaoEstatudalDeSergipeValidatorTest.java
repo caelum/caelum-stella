c7c39dca5438be84b641e5390bb2517e688b23ef
Leonardo Bessa <leobessa@gmail.com>
2008-05-19 17:01:36 +0000

    Adicionando logica de validacao de IE para Sergipe

---
+++ stella-core/src/test/java/br/com/caelum/stella/validation/InscricaoEstatudalDeSergipeValidatorTest.java
@@ -0,0 +1,209 @@
+package br.com.caelum.stella.validation;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Test;
+
+import br.com.caelum.stella.MessageProducer;
+import br.com.caelum.stella.ValidationMessage;
+import br.com.caelum.stella.validation.error.IEError;
+
+
+public class InscricaoEstatudalDeSergipeValidatorTest {
+	
+	// 27123456-3
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldNotValidateIEWithInvalidCharacter() {
+		Mockery mockery = new Mockery();
+		final MessageProducer messageProducer = mockery.mock(MessageProducer.class);
+		
+		mockery.checking(new Expectations(){{
+			exactly(1).of(messageProducer).getMessage(IEError.INVALID_DIGITS);
+		}});
+		Validator validator = new InscricaoEstatudalDeSergipeValidator(messageProducer,false);
+		try {
+			validator.assertValid("27123456a3");
+			fail();
+		} catch (InvalidStateException e) {
+			assertTrue(e.getInvalidMessages().size()==1);
+		}
+		
+		mockery.assertIsSatisfied();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldNotValidateIEWithLessDigitsThanAllowed() {
+		Mockery mockery = new Mockery();
+		final MessageProducer messageProducer = mockery.mock(MessageProducer.class);
+		
+		mockery.checking(new Expectations(){{
+			exactly(1).of(messageProducer).getMessage(IEError.INVALID_DIGITS);
+		}});
+		Validator validator = new InscricaoEstatudalDeSergipeValidator(messageProducer,false);
+		try {
+			validator.assertValid("27123453");
+			fail();
+		} catch (InvalidStateException e) {
+			assertTrue(e.getInvalidMessages().size()==1);
+		}
+		
+		mockery.assertIsSatisfied();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldNotValidateIEWithMoreDigitsThanAlowed() {
+		Mockery mockery = new Mockery();
+		final MessageProducer messageProducer = mockery.mock(MessageProducer.class);
+		
+		mockery.checking(new Expectations(){{
+			exactly(1).of(messageProducer).getMessage(IEError.INVALID_DIGITS);
+		}});
+		Validator validator = new InscricaoEstatudalDeSergipeValidator(messageProducer,false);
+
+		String value = "2712345638";
+		try {
+			validator.assertValid(value);
+			fail();
+		} catch (InvalidStateException e) {
+			assertTrue(e.getInvalidMessages().size()==1);
+		}
+		
+		mockery.assertIsSatisfied();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldNotValidateIEWithCheckDigitsWithFirstCheckDigitWrong() {
+		Mockery mockery = new Mockery();
+		final MessageProducer messageProducer = mockery.mock(MessageProducer.class);
+		
+		mockery.checking(new Expectations(){{
+			exactly(1).of(messageProducer).getMessage(IEError.INVALID_CHECK_DIGITS);
+		}});
+		Validator validator = new InscricaoEstatudalDeSergipeValidator(messageProducer,false);
+		
+		// VALID IE = 27123456-3
+		String value = "271234562";
+		try {
+			validator.assertValid(value);
+			fail();
+		} catch (InvalidStateException e) {
+			assertTrue(e.getInvalidMessages().size()==1);
+		}
+
+		mockery.assertIsSatisfied();
+	}
+	
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldValidateValidIE() {
+		Mockery mockery = new Mockery();
+		final MessageProducer messageProducer = mockery.mock(MessageProducer.class);
+		mockery.checking(new Expectations(){{
+			
+		}});
+		Validator validator = new InscricaoEstatudalDeSergipeValidator(messageProducer,false);
+		
+		List<ValidationMessage> errors;
+		
+		String value = "271234563";
+		try {
+			validator.assertValid(value);
+		} catch (InvalidStateException e) {
+			fail();
+		}
+		errors = validator.invalidMessagesFor(value);
+		assertTrue(errors.isEmpty());
+		
+		mockery.assertIsSatisfied();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldValidateNullIE() {
+		Mockery mockery = new Mockery();
+		final MessageProducer messageProducer = mockery.mock(MessageProducer.class);
+		mockery.checking(new Expectations(){{
+			
+		}});
+		Validator validator = new InscricaoEstatudalDeSergipeValidator(messageProducer,false);
+		
+		List<ValidationMessage> errors;
+		String value = null;
+		try {
+			validator.assertValid(value);
+		} catch (InvalidStateException e) {
+			fail();
+		}
+		errors = validator.invalidMessagesFor(value);
+		assertTrue(errors.isEmpty());
+		
+		mockery.assertIsSatisfied();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldValidateValidFormattedIE(){
+		Mockery mockery = new Mockery();
+		final MessageProducer messageProducer = mockery
+				.mock(MessageProducer.class);
+
+		mockery.checking(new Expectations() {
+			{
+				
+			}
+		});
+		Validator validator = new InscricaoEstatudalDeSergipeValidator(messageProducer,true);
+		List<ValidationMessage> errors;
+
+		// VALID IE = 27123456-3
+		String value = "27123456-3";
+		try {
+			validator.assertValid(value);
+		} catch (InvalidStateException e) {
+			fail();
+		}
+		errors = validator.invalidMessagesFor(value);
+		assertTrue(errors.isEmpty());
+		
+		mockery.assertIsSatisfied();
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldNotValidateValidUnformattedIE(){
+		Mockery mockery = new Mockery();
+		final MessageProducer messageProducer = mockery
+				.mock(MessageProducer.class);
+
+		mockery.checking(new Expectations() {
+			{
+				exactly(1).of(messageProducer).getMessage(
+						IEError.INVALID_FORMAT);
+			}
+		});
+		Validator validator = new InscricaoEstatudalDeSergipeValidator(messageProducer,true);
+
+		// VALID IE = 27123456-3
+		String value = "2712345-63";
+		try {
+			validator.assertValid(value);
+			fail();
+		} catch (InvalidStateException e) {
+			assertTrue(e.getInvalidMessages().size()==1);
+		}
+		
+		mockery.assertIsSatisfied();
+	}
+
+}
