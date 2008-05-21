de74d8cf0f0681779fafb935cbb9f907eeeb6909
Fabio Kung <fabio.kung@gmail.com>
2008-05-21 20:13:01 +0000

    reformatando

--- stella-core/src/main/java/br/com/caelum/stella/validation/InscricaoEstatudalDeRoraimaValidator.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/InscricaoEstatudalDeRoraimaValidator.java
@@ -1,15 +1,14 @@
 package br.com.caelum.stella.validation;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-
 import br.com.caelum.stella.MessageProducer;
 import br.com.caelum.stella.SimpleMessageProducer;
 import br.com.caelum.stella.validation.error.IEError;
 
-public class InscricaoEstatudalDeRoraimaValidator extends
-        BaseValidator<String> {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class InscricaoEstatudalDeRoraimaValidator extends BaseValidator<String> {
 
     private static final int MOD = 9;
 
@@ -18,22 +17,16 @@ public class InscricaoEstatudalDeRoraimaValidator extends
 
     private static final int DVX_POSITION = MISSING_LEFT_SIDE_ZEROS.length() + 9;
     private static final Integer[] DVX_MULTIPLIERS = IEConstraints.P5;
+    private static final RotinaDeDigitoVerificador[] ROTINAS = {IEConstraints.Rotina.D, IEConstraints.Rotina.POS_IE};
+    private static final DigitoVerificadorInfo DVX_INFO =
+            new DigitoVerificadorInfo(0, ROTINAS, MOD, DVX_MULTIPLIERS, DVX_POSITION);
 
-    private static final RotinaDeDigitoVerificador[] ROTINAS = {
-            IEConstraints.Rotina.D, IEConstraints.Rotina.POS_IE };
+    public static final Pattern FORMATED = Pattern.compile("([2][4])(\\d{6})[-](\\d{1})");
+    public static final Pattern UNFORMATED = Pattern.compile("([2][4])(\\d{6})(\\d{1})");
 
-    private static final DigitoVerificadorInfo DVX_INFO = new DigitoVerificadorInfo(
-            0, ROTINAS, MOD, DVX_MULTIPLIERS, DVX_POSITION);
+    private final ValidadorDeDV validadorDeDV = new ValidadorDeDV(DVX_INFO);
+    private final boolean formatted;
 
-    private static final ValidadorDeDV DVX_CHECKER = new ValidadorDeDV(DVX_INFO);
-
-    private final boolean isFormatted;
-
-    public static final Pattern FORMATED = Pattern
-            .compile("([2][4])(\\d{6})[-](\\d{1})");
-    public static final Pattern UNFORMATED = Pattern
-            .compile("([2][4])(\\d{6})(\\d{1})");
-    
     /**
      * Este considera, por padrão, que as cadeias estão formatadas e utiliza um
      * {@linkplain SimpleMessageProducer} para geração de mensagens.
@@ -45,19 +38,16 @@ public class InscricaoEstatudalDeRoraimaValidator extends
     /**
      * O validador utiliza um {@linkplain SimpleMessageProducer} para geração de
      * mensagens.
-     * 
-     * @param isFormatted
-     *                considerar cadeia formatada quando <code>true</code>
+     *
+     * @param formatted considerar cadeia formatada quando <code>true</code>
      */
-    public InscricaoEstatudalDeRoraimaValidator(boolean isFormatted) {
-        super();
-        this.isFormatted = isFormatted;
+    public InscricaoEstatudalDeRoraimaValidator(boolean formatted) {
+        this.formatted = formatted;
     }
 
-    public InscricaoEstatudalDeRoraimaValidator(
-            MessageProducer messageProducer, boolean isFormatted) {
+    public InscricaoEstatudalDeRoraimaValidator(MessageProducer messageProducer, boolean formatted) {
         super(messageProducer);
-        this.isFormatted = isFormatted;
+        this.formatted = formatted;
     }
 
     @Override
@@ -77,7 +67,7 @@ public class InscricaoEstatudalDeRoraimaValidator extends
 
     private String checkForCorrectFormat(String ie, List<InvalidValue> errors) {
         String unformatedIE = null;
-        if (isFormatted) {
+        if (formatted) {
             if (!(FORMATED.matcher(ie).matches())) {
                 errors.add(IEError.INVALID_FORMAT);
             }
@@ -93,7 +83,7 @@ public class InscricaoEstatudalDeRoraimaValidator extends
 
     private boolean hasValidCheckDigits(String value) {
         String testedValue = MISSING_LEFT_SIDE_ZEROS + value;
-        return DVX_CHECKER.DVisValid(testedValue);
+        return this.validadorDeDV.isDVValid(testedValue);
     }
 
 }
