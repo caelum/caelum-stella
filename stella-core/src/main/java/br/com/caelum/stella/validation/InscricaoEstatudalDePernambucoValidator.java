2c77b8afe72251dd320620c1a75f4dbc623b07e2
Fabio Kung <fabio.kung@gmail.com>
2008-05-21 20:03:09 +0000

    removendo inner class para simplificar (algum motivo para ela existir?)

--- stella-core/src/main/java/br/com/caelum/stella/validation/InscricaoEstatudalDePernambucoValidator.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/InscricaoEstatudalDePernambucoValidator.java
@@ -2,15 +2,14 @@ package br.com.caelum.stella.validation;
 
 import br.com.caelum.stella.MessageProducer;
 import br.com.caelum.stella.SimpleMessageProducer;
-import br.com.caelum.stella.ValidationMessage;
 
 import java.util.List;
 
-public class InscricaoEstatudalDePernambucoValidator implements Validator<String> {
+public class InscricaoEstatudalDePernambucoValidator extends BaseValidator<String> {
 
     private final InscricaoEstatudalDePernambucoAntigaValidator antigaValidator;
     private final InscricaoEstatudalDePernambucoNovaValidator novaValidator;
-    private final BaseValidator<String> baseValidator;
+    private final boolean formatted;
 
     /**
      * Este considera, por padrão, que as cadeias estão formatadas e utiliza um
@@ -24,51 +23,35 @@ public class InscricaoEstatudalDePernambucoValidator implements Validator<String
      * O validador utiliza um {@linkplain SimpleMessageProducer} para geração de
      * mensagens.
      *
-     * @param isFormatted considerar cadeia formatada quando <code>true</code>
+     * @param formatted considerar cadeia formatada quando <code>true</code>
      */
-    public InscricaoEstatudalDePernambucoValidator(boolean isFormatted) {
-        this(new SimpleMessageProducer(), isFormatted);
+    public InscricaoEstatudalDePernambucoValidator(boolean formatted) {
+        this(new SimpleMessageProducer(), formatted);
     }
 
-    public InscricaoEstatudalDePernambucoValidator(
-            MessageProducer messageProducer, boolean isFormatted) {
-        antigaValidator = new InscricaoEstatudalDePernambucoAntigaValidator(
-                null, isFormatted);
-        novaValidator = new InscricaoEstatudalDePernambucoNovaValidator(null,
-                isFormatted);
-        final boolean finalIsFormatted = isFormatted;
-        this.baseValidator = new BaseValidator<String>(messageProducer) {
+    public InscricaoEstatudalDePernambucoValidator(MessageProducer messageProducer, boolean formatted) {
+        super(messageProducer);
+        this.formatted = formatted;
+        antigaValidator = new InscricaoEstatudalDePernambucoAntigaValidator(messageProducer, formatted);
+        novaValidator = new InscricaoEstatudalDePernambucoNovaValidator(messageProducer, formatted);
+    }
 
-            @Override
-            protected List<InvalidValue> getInvalidValues(String value) {
-                List<InvalidValue> result = null;
-                if (finalIsFormatted) {
-                    if (InscricaoEstatudalDePernambucoAntigaValidator.FORMATED
-                            .matcher(value).matches()) {
-                        result = antigaValidator.getInvalidValues(value);
-                    } else {
-                        result = novaValidator.getInvalidValues(value);
-                    }
-                } else {
-                    if (InscricaoEstatudalDePernambucoAntigaValidator.UNFORMATED
-                            .matcher(value).matches()) {
-                        result = antigaValidator.getInvalidValues(value);
-                    } else {
-                        result = novaValidator.getInvalidValues(value);
-                    }
-                }
-                return result;
+    protected List<InvalidValue> getInvalidValues(String value) {
+        List<InvalidValue> result = null;
+        // FIXME usar mais polimorfismo
+        if (this.formatted) {
+            if (InscricaoEstatudalDePernambucoAntigaValidator.FORMATED.matcher(value).matches()) {
+                result = antigaValidator.getInvalidValues(value);
+            } else {
+                result = novaValidator.getInvalidValues(value);
             }
-
-        };
+        } else {
+            if (InscricaoEstatudalDePernambucoAntigaValidator.UNFORMATED.matcher(value).matches()) {
+                result = antigaValidator.getInvalidValues(value);
+            } else {
+                result = novaValidator.getInvalidValues(value);
+            }
+        }
+        return result;
     }
-
-    public void assertValid(String value) {
-        baseValidator.assertValid(value);
-    }
-
-    public List<ValidationMessage> invalidMessagesFor(String value) {
-        return baseValidator.invalidMessagesFor(value);
-    }
-
 }
