5d26b792ce346119c0db789e52156eb59423a27e
Fabio Kung <fabio.kung@gmail.com>
2008-05-21 19:18:39 +0000

    reformatando, arrumando javadoc

--- stella-core/src/main/java/br/com/caelum/stella/validation/IEConstraints.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/IEConstraints.java
@@ -2,39 +2,25 @@ package br.com.caelum.stella.validation;
 
 /**
  * Documentação que pode ser consultada:
- * {@link http://www.pfe.fazenda.sp.gov.br/consist_ie.shtm}
- * 
+ * <a href="http://www.pfe.fazenda.sp.gov.br/consist_ie.shtm">http://www.pfe.fazenda.sp.gov.br/consist_ie.shtm</a>
+ *
  * @author leobessa
- * 
  */
 class IEConstraints {
 
-    public static final Integer[] P1 = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2,
-            0 };
-    public static final Integer[] P2 = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 0,
-            0 };
-    public static final Integer[] P3 = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 0,
-            2 };
-    public static final Integer[] P4 = { 0, 0, 0, 0, 0, 0, 0, 0, 6, 5, 4, 3, 2,
-            0 };
-    public static final Integer[] P5 = { 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8,
-            0 };
-    public static final Integer[] P6 = { 0, 0, 0, 9, 8, 0, 0, 7, 6, 5, 4, 3, 2,
-            0 };
-    public static final Integer[] P7 = { 5, 4, 3, 2, 1, 9, 8, 7, 6, 5, 4, 3, 2,
-            0 };
-    public static final Integer[] P8 = { 8, 7, 6, 5, 4, 3, 2, 7, 6, 5, 4, 3, 2,
-            0 };
-    public static final Integer[] P9 = { 7, 6, 5, 4, 3, 2, 7, 6, 5, 4, 3, 2, 0,
-            0 };
-    public static final Integer[] P10 = { 0, 1, 2, 1, 1, 2, 1, 2, 1, 2, 1, 2,
-            0, 0 };
-    public static final Integer[] P11 = { 0, 3, 2, 11, 10, 9, 8, 7, 6, 5, 4, 3,
-            2, 0 };
-    public static final Integer[] P12 = { 0, 0, 1, 3, 4, 5, 6, 7, 8, 10, 0, 0,
-            0, 0 };
-    public static final Integer[] P13 = { 0, 0, 3, 2, 10, 9, 8, 7, 6, 5, 4, 3,
-            2, 0 };
+    public static final Integer[] P1 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 0};
+    public static final Integer[] P2 = {5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2, 0, 0};
+    public static final Integer[] P3 = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 0, 2};
+    public static final Integer[] P4 = {0, 0, 0, 0, 0, 0, 0, 0, 6, 5, 4, 3, 2, 0};
+    public static final Integer[] P5 = {0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 0};
+    public static final Integer[] P6 = {0, 0, 0, 9, 8, 0, 0, 7, 6, 5, 4, 3, 2, 0};
+    public static final Integer[] P7 = {5, 4, 3, 2, 1, 9, 8, 7, 6, 5, 4, 3, 2, 0};
+    public static final Integer[] P8 = {8, 7, 6, 5, 4, 3, 2, 7, 6, 5, 4, 3, 2, 0};
+    public static final Integer[] P9 = {7, 6, 5, 4, 3, 2, 7, 6, 5, 4, 3, 2, 0, 0};
+    public static final Integer[] P10 = {0, 1, 2, 1, 1, 2, 1, 2, 1, 2, 1, 2, 0, 0};
+    public static final Integer[] P11 = {0, 3, 2, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 0};
+    public static final Integer[] P12 = {0, 0, 1, 3, 4, 5, 6, 7, 8, 10, 0, 0, 0, 0};
+    public static final Integer[] P13 = {0, 0, 3, 2, 10, 9, 8, 7, 6, 5, 4, 3, 2, 0};
 
     public enum Rotina implements RotinaDeDigitoVerificador {
         A {
