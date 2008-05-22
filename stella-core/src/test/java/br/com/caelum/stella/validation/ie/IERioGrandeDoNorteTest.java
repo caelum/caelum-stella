a51b0aeb14c66ebb193a08615ae657e9b650b54c
Leonardo Bessa <leobessa@gmail.com>
2008-05-22 15:25:13 +0000

    Logica de Validacao de IE de RN (Rio Grande do Norte)

---
+++ stella-core/src/test/java/br/com/caelum/stella/validation/ie/IERioGrandeDoNorteTest.java
@@ -0,0 +1,330 @@
+package br.com.caelum.stella.validation.ie;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.List;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.Test;
+
+import br.com.caelum.stella.MessageProducer;
+import br.com.caelum.stella.ValidationMessage;
+import br.com.caelum.stella.validation.error.IEError;
+import br.com.caelum.stella.validation.ie.IERioGrandeDoNorteValidator;
+import br.com.caelum.stella.validation.Validator;
+import br.com.caelum.stella.validation.InvalidStateException;
+
+public class IERioGrandeDoNorteTest {
+
+    /*
+     * IE validas 20.040.040-1(9 dígitos) ou 20.0.040.040-0(10 dígitos)
+     */
+
+    private static final String wrongCheckDigitUnformattedStringWithNineDigits = "200400400";
+    private static final String wrongCheckDigitUnformattedStringWithTenDigits = "2000400403";
+    private static final String validUnformattedStringWithNineDigits = "200400401";
+    private static final String validUnformattedStringWithTenDigits  = "2000400400";
+    private static final String validFormattedStringWithNineDigits =  "20.040.040-1";
+    private static final String validFormattedStringWithTenDigits = "20.0.040.040-0";
+
+    private Validator<String> newValidator() {
+        return new IERioGrandeDoNorteValidator();
+    }
+
+    @Test
+    public void shouldHaveDefaultConstructorThatUsesSimpleMessageProducerAndAssumesThatStringIsFormatted() {
+        newValidator().assertValid(validFormattedStringWithNineDigits);
+
+        try {
+            newValidator().assertValid(validFormattedStringWithTenDigits);
+        } catch (RuntimeException e) {
+            if (e instanceof InvalidStateException) {
+                InvalidStateException invalidStateException = (InvalidStateException) e;
+                String expected = "IEError : INVALID CHECK DIGITS";
+                assertEquals(expected, invalidStateException
+                        .getInvalidMessages().get(0).getMessage());
+            } else {
+                fail();
+            }
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldNotValidateIEWithInvalidCharacter() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+
+        mockery.checking(new Expectations() {
+            {
+                exactly(1).of(messageProducer).getMessage(
+                        IEError.INVALID_DIGITS);
+            }
+        });
+        Validator validator = new IERioGrandeDoNorteValidator(messageProducer,
+                false);
+        try {
+            validator.assertValid("2004a0401");
+            fail();
+        } catch (InvalidStateException e) {
+            assertTrue(e.getInvalidMessages().size() == 1);
+        }
+
+        mockery.assertIsSatisfied();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldNotValidateIEWithLessDigitsThanAllowed() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+
+        mockery.checking(new Expectations() {
+            {
+                exactly(1).of(messageProducer).getMessage(
+                        IEError.INVALID_DIGITS);
+            }
+        });
+        Validator validator = new IERioGrandeDoNorteValidator(messageProducer,
+                false);
+        try {
+            validator.assertValid("20040040");
+            fail();
+        } catch (InvalidStateException e) {
+            assertTrue(e.getInvalidMessages().size() == 1);
+        }
+
+        mockery.assertIsSatisfied();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldNotValidateIEWithMoreDigitsThanAlowed() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+
+        mockery.checking(new Expectations() {
+            {
+                exactly(1).of(messageProducer).getMessage(
+                        IEError.INVALID_DIGITS);
+            }
+        });
+        Validator validator = new IERioGrandeDoNorteValidator(messageProducer,
+                false);
+
+        String value = "20004004005";
+        try {
+            validator.assertValid(value);
+            fail();
+        } catch (InvalidStateException e) {
+            assertTrue(e.getInvalidMessages().size() == 1);
+        }
+
+        mockery.assertIsSatisfied();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldNotValidateIEWithNineDigitsAndCheckDigitsWithCheckDigitWrong() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+
+        mockery.checking(new Expectations() {
+            {
+                exactly(1).of(messageProducer).getMessage(
+                        IEError.INVALID_CHECK_DIGITS);
+            }
+        });
+        Validator validator = new IERioGrandeDoNorteValidator(messageProducer,
+                false);
+
+        String value = wrongCheckDigitUnformattedStringWithNineDigits;
+        try {
+            validator.assertValid(value);
+            fail();
+        } catch (InvalidStateException e) {
+            assertTrue(e.getInvalidMessages().size() == 1);
+        }
+
+        mockery.assertIsSatisfied();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldNotValidateIEWithTenDigitsAndCheckDigitsWithCheckDigitWrong() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+
+        mockery.checking(new Expectations() {
+            {
+                exactly(1).of(messageProducer).getMessage(
+                        IEError.INVALID_CHECK_DIGITS);
+            }
+        });
+        Validator validator = new IERioGrandeDoNorteValidator(messageProducer,
+                false);
+
+        String value = wrongCheckDigitUnformattedStringWithTenDigits;
+        try {
+            validator.assertValid(value);
+            fail();
+        } catch (InvalidStateException e) {
+            assertTrue(e.getInvalidMessages().size() == 1);
+        }
+
+        mockery.assertIsSatisfied();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldValidateValidIEWithNineDigits() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+        mockery.checking(new Expectations());
+        Validator validator = new IERioGrandeDoNorteValidator(messageProducer,
+                false);
+
+        List<ValidationMessage> errors;
+
+        String validValue = validUnformattedStringWithNineDigits;
+        try {
+            validator.assertValid(validValue);
+        } catch (InvalidStateException e) {
+            fail();
+        }
+        errors = validator.invalidMessagesFor(validValue);
+        assertTrue(errors.isEmpty());
+
+        mockery.assertIsSatisfied();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldValidateValidIEWithTenDigits() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+        mockery.checking(new Expectations());
+        Validator validator = new IERioGrandeDoNorteValidator(messageProducer,
+                false);
+
+        List<ValidationMessage> errors;
+
+        String validValue = validUnformattedStringWithTenDigits;
+        try {
+            validator.assertValid(validValue);
+        } catch (InvalidStateException e) {
+            fail();
+        }
+        errors = validator.invalidMessagesFor(validValue);
+        assertTrue(errors.isEmpty());
+
+        mockery.assertIsSatisfied();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldValidateNullIE() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+        mockery.checking(new Expectations());
+        Validator validator = new IERioGrandeDoNorteValidator(messageProducer,
+                false);
+
+        List<ValidationMessage> errors;
+        String value = null;
+        try {
+            validator.assertValid(value);
+        } catch (InvalidStateException e) {
+            fail();
+        }
+        errors = validator.invalidMessagesFor(value);
+        assertTrue(errors.isEmpty());
+
+        mockery.assertIsSatisfied();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldValidateValidFormattedIEWithNineDigits() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+
+        mockery.checking(new Expectations());
+        Validator validator = new IERioGrandeDoNorteValidator(messageProducer,
+                true);
+        List<ValidationMessage> errors;
+
+        String value = validFormattedStringWithNineDigits;
+        try {
+            validator.assertValid(value);
+        } catch (InvalidStateException e) {
+            fail();
+        }
+        errors = validator.invalidMessagesFor(value);
+        assertTrue(errors.isEmpty());
+        mockery.assertIsSatisfied();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldValidateValidFormattedIEWithTenDigits() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+
+        mockery.checking(new Expectations());
+        Validator validator = new IERioGrandeDoNorteValidator(messageProducer,
+                true);
+        List<ValidationMessage> errors;
+
+        String value = validFormattedStringWithTenDigits;
+        try {
+            validator.assertValid(value);
+        } catch (InvalidStateException e) {
+            fail();
+        }
+        errors = validator.invalidMessagesFor(value);
+        assertTrue(errors.isEmpty());
+        mockery.assertIsSatisfied();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void shouldNotValidateValidUnformattedIE() {
+        Mockery mockery = new Mockery();
+        final MessageProducer messageProducer = mockery
+                .mock(MessageProducer.class);
+
+        mockery.checking(new Expectations() {
+            {
+                exactly(1).of(messageProducer).getMessage(
+                        IEError.INVALID_FORMAT);
+            }
+        });
+        Validator validator = new IEMatoGrossoValidator(messageProducer, true);
+
+        // VALID IE = "20.040.040-1"
+        String value = "20.040=040-1";
+        try {
+            validator.assertValid(value);
+            fail();
+        } catch (InvalidStateException e) {
+            assertTrue(e.getInvalidMessages().size() == 1);
+        }
+
+        mockery.assertIsSatisfied();
+    }
+
+}
