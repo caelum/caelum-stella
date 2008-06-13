e0acbe610c0a85ed0051d6ea791be7db14a2b1ed
Fabio Kung <fabio.kung@gmail.com>
2008-06-13 17:57:53 +0000

    testes faltando

--- stella-boleto/src/test/java/br/com/caelum/stella/boleto/EmissorTest.java
+++ stella-boleto/src/test/java/br/com/caelum/stella/boleto/EmissorTest.java
@@ -1,8 +1,6 @@
 package br.com.caelum.stella.boleto;
 
 import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
 
 /**
@@ -18,8 +16,19 @@ public class EmissorTest {
     }
 
     @Test
-    public void testesSimilaresParaOutrosNumerosFormatados() {
-        fail("pendente");
+    public void nossoNumeroFormatadoDeveTerDezDigitos() {
+        Emissor emissor = Emissor.newEmissor().withNossoNumero("12345");
+        String numeroFormatado = emissor.getNossoNumeroFormatado();
+        assertEquals(10, numeroFormatado.length());
+        assertEquals("0000012345", numeroFormatado);
+    }
+
+    @Test
+    public void numeroDoConvenioFormatadoDeveTerSeteDigitos() {
+        Emissor emissor = Emissor.newEmissor().withNumConvenio("1234");
+        String numeroFormatado = emissor.getNumConvenioFormatado();
+        assertEquals(7, numeroFormatado.length());
+        assertEquals("0001234", numeroFormatado);
     }
 
 }
