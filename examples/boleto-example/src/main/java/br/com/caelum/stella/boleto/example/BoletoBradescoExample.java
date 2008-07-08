ccaa85a05abf8bcc66c367c7fe309b771e021f8d
Caue Guerra <caueguerra@gmail.com>
2008-07-08 18:22:34 +0000

    exemplos, tinha faltado String para int

--- examples/boleto-example/src/main/java/br/com/caelum/stella/boleto/example/BoletoBradescoExample.java
+++ examples/boleto-example/src/main/java/br/com/caelum/stella/boleto/example/BoletoBradescoExample.java
@@ -9,40 +9,43 @@ import br.com.caelum.stella.boleto.bancos.Bradesco;
 import br.com.caelum.stella.boleto.transformer.BoletoGenerator;
 
 public class BoletoBradescoExample {
-	public static void main(String[] args) {
-		Datas datas = Datas.newDatas().withDocumento(1, 5, 2008)
-				.withProcessamento(1, 5, 2008).withVencimento(2, 5, 2008);
+    public static void main(String[] args) {
+	Datas datas = Datas.newDatas().withDocumento(1, 5, 2008)
+		.withProcessamento(1, 5, 2008).withVencimento(2, 5, 2008);
 
-		Emissor emissor = Emissor.newEmissor().withCedente("Fulano de Tal")
-				.withAgencia("2345").withDvAgencia("6").withContaCorrente(12345)
-                .withNumConvenio("1234567").withDvContaCorrete("1").withCarteira("22")
-                .withNossoNumero("0009050987");
+	Emissor emissor = Emissor.newEmissor().withCedente("Fulano de Tal")
+		.withAgencia(2345).withDvAgencia("6").withContaCorrente(12345)
+		.withNumConvenio("1234567").withDvContaCorrete("1")
+		.withCarteira(22).withNossoNumero("0009050987");
 
-		Sacado sacado = Sacado.newSacado().withNome("Fulano da Silva")
-                .withCpf("111.222.333-12").withEndereco("Av dos testes, 111 apto 333")
-				.withBairro("Bairro Teste").withCep("01234-111").withCidade("São Paulo")
-                .withUf("SP");
+	Sacado sacado = Sacado.newSacado().withNome("Fulano da Silva").withCpf(
+		"111.222.333-12").withEndereco("Av dos testes, 111 apto 333")
+		.withBairro("Bairro Teste").withCep("01234-111").withCidade(
+			"São Paulo").withUf("SP");
 
-		Banco banco = new Bradesco();
+	Banco banco = new Bradesco();
 
-		Boleto boleto = Boleto.newBoleto().withBanco(banco).withDatas(datas)
-				.withDescricoes("descricao 1", "descricao 2", "descricao 3", "descricao 4", "descricao 5")
-                .withEmissor(emissor).withSacado(sacado).withValorBoleto("200.00").withNoDocumento("1234")
-                .withInstrucoes("instrucao 1", "instrucao 2", "instrucao 3", "instrucao 4", "instrucao 5")
-				.withLocaisDePagamento("local 1", "local 2").withNoDocumento("4343");
+	Boleto boleto = Boleto.newBoleto().withBanco(banco).withDatas(datas)
+		.withDescricoes("descricao 1", "descricao 2", "descricao 3",
+			"descricao 4", "descricao 5").withEmissor(emissor)
+		.withSacado(sacado).withValorBoleto("200.00").withNoDocumento(
+			"1234").withInstrucoes("instrucao 1", "instrucao 2",
+			"instrucao 3", "instrucao 4", "instrucao 5")
+		.withLocaisDePagamento("local 1", "local 2").withNoDocumento(
+			"4343");
 
-		BoletoGenerator gerador = new BoletoGenerator(boleto);
+	BoletoGenerator gerador = new BoletoGenerator(boleto);
 
-		// Para gerar um boleto em PDF
-		gerador.toPDF("BoletoBradesco.pdf");
+	// Para gerar um boleto em PDF
+	gerador.toPDF("BoletoBradesco.pdf");
 
-		// Para gerar um boleto em PNG
-		gerador.toPNG("BoletoBradesco.png");
+	// Para gerar um boleto em PNG
+	gerador.toPNG("BoletoBradesco.png");
 
-		// Para gerar um array de bytes a partir de um PDF
-		byte[] bPDF = gerador.toPDF();
+	// Para gerar um array de bytes a partir de um PDF
+	byte[] bPDF = gerador.toPDF();
 
-		// Para gerar um array de bytes a partir de um PNG
-		byte[] bPNG = gerador.toPNG();
-	}
+	// Para gerar um array de bytes a partir de um PNG
+	byte[] bPNG = gerador.toPNG();
+    }
 }
