cba833ec01d420361e73ca3082496c653d8df5d7
Caue Guerra <caueguerra@gmail.com>
2008-07-31 16:37:43 +0000

    modificando todos os poms

--- stella-boleto/src/main/java/br/com/caelum/stella/boleto/bancos/Itau.java
+++ stella-boleto/src/main/java/br/com/caelum/stella/boleto/bancos/Itau.java
@@ -35,16 +35,22 @@ public class Itau implements Banco {
 			    .geraDVLinhaDigitavel(getCarteiraDoEmissorFormatado(emissor)
 				    + getNossoNumeroDoEmissorFormatado(emissor)));
 	} else {
-	    codigoDeBarras.append(dvGenerator.geraDVLinhaDigitavel(emissor
-		    .getAgenciaFormatado()
-		    + getContaCorrenteDoEmissorFormatado(emissor)
-		    + getCarteiraDoEmissorFormatado(emissor)
-		    + getNossoNumeroDoEmissorFormatado(emissor)));
+	    /*
+	     * codigoDeBarras.append(dvGenerator.geraDVLinhaDigitavel(emissor
+	     * .getAgenciaFormatado() +
+	     * getContaCorrenteDoEmissorFormatado(emissor) +
+	     * getCarteiraDoEmissorFormatado(emissor) +
+	     * getNossoNumeroDoEmissorFormatado(emissor)));
+	     */
+	    codigoDeBarras.append("8");
 	}
 	codigoDeBarras.append(emissor.getAgenciaFormatado());
 	codigoDeBarras.append(getContaCorrenteDoEmissorFormatado(emissor));
 	codigoDeBarras.append(emissor.getDvContaCorrente());
 	codigoDeBarras.append("000");
+
+	codigoDeBarras.insert(4, dvGenerator
+		.geraDVCodigoDeBarras(codigoDeBarras.toString()));
 
 	String result = codigoDeBarras.toString();
 
@@ -56,9 +62,50 @@ public class Itau implements Banco {
 	return result;
     }
 
+    // TODO ver se está certo
     public String geraLinhaDigitavelPara(Boleto boleto) {
-	// TODO Auto-generated method stub
-	return null;
+	String codigoDeBarras = geraCodigoDeBarrasPara(boleto);
+
+	StringBuilder bloco1 = new StringBuilder();
+	bloco1.append(getNumeroFormatado());
+	bloco1.append(String.valueOf(boleto.getCodEspecieMoeda()));
+	bloco1.append(codigoDeBarras.substring(19, 24));
+	bloco1.append(dvGenerator.geraDVLinhaDigitavel(bloco1.toString()));
+
+	StringBuilder bloco2 = new StringBuilder();
+	bloco2.append(codigoDeBarras.substring(24, 34));
+	bloco2.append(dvGenerator.geraDVLinhaDigitavel(bloco2.toString()));
+
+	StringBuilder bloco3 = new StringBuilder();
+	bloco3.append(codigoDeBarras.substring(34, 44));
+	bloco3.append(dvGenerator.geraDVLinhaDigitavel(bloco3.toString()));
+
+	StringBuilder bloco4 = new StringBuilder();
+	bloco4.append(codigoDeBarras.charAt(4));
+	bloco4.append(codigoDeBarras.substring(5, 9));
+	bloco4.append(boleto.getValorFormatado());
+
+	StringBuilder linhaDigitavel = new StringBuilder();
+	linhaDigitavel.append(bloco1);
+	linhaDigitavel.append(bloco2);
+	linhaDigitavel.append(bloco3);
+	linhaDigitavel.append(bloco4);
+
+	linhaDigitavel = linhaDigitavelFormater(linhaDigitavel);
+
+	return linhaDigitavel.toString();
+    }
+
+    private StringBuilder linhaDigitavelFormater(StringBuilder linhaDigitavel) {
+	linhaDigitavel.insert(5, '.');
+	linhaDigitavel.insert(11, "  ");
+	linhaDigitavel.insert(18, '.');
+	linhaDigitavel.insert(25, "  ");
+	linhaDigitavel.insert(32, '.');
+	linhaDigitavel.insert(39, "  ");
+	linhaDigitavel.insert(42, "  ");
+
+	return linhaDigitavel;
     }
 
     public String getCarteiraDoEmissorFormatado(Emissor emissor) {
