75d9555d03b5de71fe596b95c7cadbd9b9b39158
Caires Vinicius <cairesvs@gmail.com>
2008-10-24 15:38:45 +0000

    mascara para cnpj funcionando.

--- stella-js/src/mask.js
+++ stella-js/src/mask.js
@@ -3,6 +3,12 @@ Stella.Masker = function(){
   this.cpfFields.each(function(){
     this.mask = ['#','#','#','.','#','#','#','.','#','#','#','-','#','#'];
   });
+  this.cnpjFields = jQuery(':text.cnpj');
+  this.cnpjFields.each(function(){
+    // 00.000.000/0000-00
+    // 12345678901234
+    this.mask = ['#','#','.','#','#','#','.','#','#','#','/','#','#','#','#','-','#','#'];
+  });
 };
 
 Stella.Masker.prototype.mask = function(){
@@ -11,6 +17,12 @@ Stella.Masker.prototype.mask = function(){
   });
   this.cpfFields.each(function(){
     Stella.Masker.cpfWithDefinedValue(this);
+  });
+  this.cnpjFields.keypress(function(event){
+    Stella.Masker.cnpj(this, event);
+  });
+  this.cnpjFields.each(function(){
+    Stella.Masker.cnpjWithDefinedValue(this);
   });
 };
 
@@ -54,4 +66,47 @@ Stella.Masker.cpfWithDefinedValue = function(field){
         jQuery(input).val(newInput);
     }
   }
+}
+
+Stella.Masker.cnpj = function(field, keyEvent){
+  var input = jQuery(field);
+  var isBackspace = false;
+  var size =  input.val().length;
+  var mask = field.mask;
+  jQuery(input).attr('maxLength', mask.length);
+  
+  var kp = keyEvent.which ||keyEvent.keyCode;
+  if(kp == 8){
+    isBackspace = true;
+  }
+  
+  if(!isBackspace){
+    if(mask[size] == '.' || mask[size] == '-' || mask[size] == '/'){
+      jQuery(input).val(input.val() + mask[size]);
+    }
+  }
+}
+
+Stella.Masker.cnpjWithDefinedValue = function(field){
+  var input = jQuery(field);
+  var isBackspace = false;
+  var size = input.val().length;
+  var mask = field.mask;
+  jQuery(input).attr('maxLength', mask.length);
+  if(size >= 14 ){
+    if(!isBackspace){
+      var temp = [];
+      temp = input.val().split('');
+      var newInput = '';
+      var valueIndex = 0;
+
+      for(var size in mask){
+        newInput += mask[size] == '.' || mask[size] == '-' || mask[size] == '/'
+                    ? mask[size]
+                    : temp[valueIndex++];
+      }
+      jQuery(input).val(newInput);    
+  }
+}
+  
 }
