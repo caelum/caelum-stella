cdbabc92aae61a6148ebd15f480a1d0eb74729f7
Caue Guerra <caueguerra@gmail.com>
2009-01-09 00:10:33 +0000

    deixando o codigo de exemplo alinhado, hehe... bem coisa de fresco

--- examples/boleto-example/src/main/java/br/com/caelum/stella/boleto/example/BoletoBancoDoBrasilExample.java
+++ examples/boleto-example/src/main/java/br/com/caelum/stella/boleto/example/BoletoBancoDoBrasilExample.java
@@ -9,34 +9,43 @@ import br.com.caelum.stella.boleto.bancos.BancoDoBrasil;
 import br.com.caelum.stella.boleto.transformer.BoletoGenerator;
 
 public class BoletoBancoDoBrasilExample {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		Datas datas = Datas.newDatas().withDocumento(1, 5, 2008)
 				.withProcessamento(1, 5, 2008).withVencimento(2, 5, 2008);
 
 		Emissor emissor = Emissor.newEmissor()
 		        .withCedente("Fulano de Tal")
-		        .withAgencia(1824).withDvAgencia("4")
+		        .withAgencia(1824).withDvAgencia('4')
                 .withContaCorrente(76000)
                 .withNumConvenio(1207113)
-                .withDvContaCorrente("5")
+                .withDvContaCorrente('5')
                 .withCarteira(18)
                 .withNossoNumero(9000206);
 
-		Sacado sacado = Sacado.newSacado().withNome("Fulano da Silva").withCpf(
-				"111.222.333-12").withEndereco("Av dos testes, 111 apto 333")
-				.withBairro("Bairro Teste").withCep("01234-111").withCidade(
-						"São Paulo").withUf("SP");
+		Sacado sacado = Sacado.newSacado()
+		    .withNome("Fulano da Silva")
+		    .withCpf("111.222.333-12")
+		    .withEndereco("Av dos testes, 111 apto 333")
+			.withBairro("Bairro Teste")
+			.withCep("01234-111")
+			.withCidade("São Paulo")
+			.withUf("SP");
 
 		Banco banco = new BancoDoBrasil();
 
-		Boleto boleto = Boleto.newBoleto().withBanco(banco).withDatas(datas)
-				.withDescricoes("descricao 1", "descricao 2", "descricao 3",
-						"descricao 4", "descricao 5").withEmissor(emissor)
-				.withSacado(sacado).withValorBoleto("200.00").withNoDocumento(
-						"1234").withInstrucoes("instrucao 1", "instrucao 2",
+		Boleto boleto = Boleto.newBoleto()
+		    .withBanco(banco)
+		    .withDatas(datas)
+		    .withDescricoes("descricao 1", "descricao 2", "descricao 3",
+						"descricao 4", "descricao 5")
+			.withEmissor(emissor)
+			.withSacado(sacado)
+			.withValorBoleto("200.00")
+			.withNoDocumento("1234")
+			.withInstrucoes("instrucao 1", "instrucao 2",
 						"instrucao 3", "instrucao 4", "instrucao 5")
-				.withLocaisDePagamento("local 1", "local 2").withNoDocumento(
-						"4343");
+			.withLocaisDePagamento("local 1", "local 2")
+			.withNoDocumento("4343");
 
 		BoletoGenerator gerador = new BoletoGenerator(boleto);
 
--- examples/boleto-example/src/main/java/br/com/caelum/stella/boleto/example/BoletoBradescoExample.java
+++ examples/boleto-example/src/main/java/br/com/caelum/stella/boleto/example/BoletoBradescoExample.java
@@ -10,24 +10,43 @@ import br.com.caelum.stella.boleto.transformer.BoletoGenerator;
 
 public class BoletoBradescoExample {
     public static void main(String[] args) {
-        Datas datas = Datas.newDatas().withDocumento(1, 5, 2008).withProcessamento(1, 5, 2008).withVencimento(2, 5,
-                2008);
+        Datas datas = Datas.newDatas().withDocumento(1, 5, 2008)
+                .withProcessamento(1, 5, 2008).withVencimento(2, 5, 2008);
 
-        Emissor emissor = Emissor.newEmissor().withCedente("Fulano de Tal").withAgencia(2345).withDvAgencia("6")
-                .withContaCorrente(12345).withNumConvenio(1234567).withDvContaCorrente("1").withCarteira(22)
-                .withNossoNumero(9050987);
+        Emissor emissor = Emissor.newEmissor()
+            .withCedente("Fulano de Tal")
+            .withAgencia(2345)
+            .withDvAgencia('6')
+            .withContaCorrente(12345)
+            .withNumConvenio(1234567)
+            .withDvContaCorrente('1')
+            .withCarteira(22)
+            .withNossoNumero(9050987);
 
-        Sacado sacado = Sacado.newSacado().withNome("Fulano da Silva").withCpf("111.222.333-12").withEndereco(
-                "Av dos testes, 111 apto 333").withBairro("Bairro Teste").withCep("01234-111").withCidade("São Paulo")
-                .withUf("SP");
+        Sacado sacado = Sacado.newSacado()
+            .withNome("Fulano da Silva")
+            .withCpf("111.222.333-12")
+            .withEndereco("Av dos testes, 111 apto 333")
+            .withBairro("Bairro Teste")
+            .withCep("01234-111")
+            .withCidade("São Paulo")
+            .withUf("SP");
 
         Banco banco = new Bradesco();
 
-        Boleto boleto = Boleto.newBoleto().withBanco(banco).withDatas(datas).withDescricoes("descricao 1",
-                "descricao 2", "descricao 3", "descricao 4", "descricao 5").withEmissor(emissor).withSacado(sacado)
-                .withValorBoleto("200.00").withNoDocumento("1234").withInstrucoes("instrucao 1", "instrucao 2",
-                        "instrucao 3", "instrucao 4", "instrucao 5").withLocaisDePagamento("local 1", "local 2")
-                .withNoDocumento("4343");
+        Boleto boleto = Boleto.newBoleto()
+            .withBanco(banco)
+            .withDatas(datas)
+            .withDescricoes("descricao 1", "descricao 2", "descricao 3",
+                        "descricao 4", "descricao 5")
+            .withEmissor(emissor)
+            .withSacado(sacado)
+            .withValorBoleto("200.00")
+            .withNoDocumento("1234")
+            .withInstrucoes("instrucao 1", "instrucao 2",
+                        "instrucao 3", "instrucao 4", "instrucao 5")
+            .withLocaisDePagamento("local 1", "local 2")
+            .withNoDocumento("4343");
 
         BoletoGenerator gerador = new BoletoGenerator(boleto);
 
