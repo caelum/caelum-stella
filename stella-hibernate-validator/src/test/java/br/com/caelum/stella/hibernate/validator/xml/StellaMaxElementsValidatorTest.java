b3b0f965854310d5da05e7f6ebbe8f9abffa7b6f
Jonas Abreu <jonasabr@gmail.com>
2009-02-10 21:05:46 +0000

    Adicionando validador para Número máximo de elementos.

---
+++ stella-hibernate-validator/src/test/java/br/com/caelum/stella/hibernate/validator/xml/StellaMaxElementsValidatorTest.java
@@ -0,0 +1,91 @@
+package br.com.caelum.stella.hibernate.validator.xml;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import junit.framework.Assert;
+import net.vidageek.mirror.Mirror;
+
+import org.junit.Test;
+
+import br.com.caelum.stella.hibernate.validator.xml.MaxElements;
+import br.com.caelum.stella.hibernate.validator.xml.logic.StellaMaxElementsValidator;
+
+public class StellaMaxElementsValidatorTest {
+
+    @Test(expected = IllegalStateException.class)
+    public void testThatThrowsExceptionIfAnnotatedElementIsNotACollectionOrArray() {
+        StellaMaxElementsValidator validator = new StellaMaxElementsValidator();
+        validator.isValid("");
+    }
+
+    @Test
+    public void testThatDoesNotThrowExceptionIfAnnotatedElementIsACollection() {
+        StellaMaxElementsValidator validator = new StellaMaxElementsValidator();
+        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(MaxElements.class).atField("c"));
+        validator.isValid(new ArrayList<String>());
+    }
+
+    @Test
+    public void testThatDoesNotThrowsExceptionIfAnnotatedElementIsAnArray() {
+        StellaMaxElementsValidator validator = new StellaMaxElementsValidator();
+        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(MaxElements.class).atField("c"));
+        validator.isValid(new Integer[10]);
+    }
+
+    @Test
+    public void testThatIsInvalidIfElementNumberIsSmallerThanCollectionSize() {
+        StellaMaxElementsValidator validator = new StellaMaxElementsValidator();
+        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(MaxElements.class).atField("c"));
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("");
+        list.add("");
+        list.add("");
+        list.add("");
+        Assert.assertFalse(validator.isValid(list));
+    }
+
+    @Test
+    public void testThatIsValidIfElementNumberIsBiggerThanCollectionSize() {
+        StellaMaxElementsValidator validator = new StellaMaxElementsValidator();
+        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(MaxElements.class).atField("c"));
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("");
+        list.add("");
+        Assert.assertTrue(validator.isValid(list));
+    }
+
+    @Test
+    public void testThatIsValidIfElementNumberIsBiggerThanNonNullArrayElements() {
+        StellaMaxElementsValidator validator = new StellaMaxElementsValidator();
+        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(MaxElements.class).atField("c"));
+
+        Assert.assertTrue(validator.isValid(new Integer[100]));
+    }
+
+    @Test
+    public void testThatIsInvalidIfElementNumberIsSmallerThanNonNullArrayElements() {
+        StellaMaxElementsValidator validator = new StellaMaxElementsValidator();
+        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(MaxElements.class).atField("c"));
+
+        Integer[] array = new Integer[100];
+        array[0] = 1;
+        array[1] = 1;
+        array[2] = 1;
+        Assert.assertFalse(validator.isValid(array));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testThatThrowsExceptionIfObjectIsArrayOfPrimitiveTypes() {
+        StellaMaxElementsValidator validator = new StellaMaxElementsValidator();
+        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(MaxElements.class).atField("c"));
+
+        validator.isValid(new int[10]);
+    }
+
+    public static class AnnotatedModel {
+        @MaxElements(2)
+        public Collection<String> c;
+
+    }
+}
