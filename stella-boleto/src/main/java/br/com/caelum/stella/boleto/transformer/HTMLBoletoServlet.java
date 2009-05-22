b3e5d87209de77d0ed6e24db73478acb9944e9e0
Alberto Souza <asouza@settech.com.br>
2009-05-22 21:39:32 +0000

    - Alteração na forma de pegar as imagens no HTMLBoletoServlet

--- stella-boleto/src/main/java/br/com/caelum/stella/boleto/transformer/HTMLBoletoServlet.java
+++ stella-boleto/src/main/java/br/com/caelum/stella/boleto/transformer/HTMLBoletoServlet.java
@@ -1,9 +1,11 @@
 package br.com.caelum.stella.boleto.transformer;
 
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,36 +24,33 @@ public class HTMLBoletoServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub						
+		// TODO Auto-generated method stub
 		String tipoRecurso = request.getParameter("type");
 		String value = request.getParameter("value");
-		BufferedImage image = getCorrectImageFor(tipoRecurso,value);
+		BufferedImage image = getCorrectImageFor(tipoRecurso, value);
 		response.setContentType("image/png");
 		ImageIO.write(image, "png", response.getOutputStream());
 	}
 
 	/**
-	 * Retorna a imagem 
+	 * Retorna a imagem
+	 * 
 	 * @param urlSolicitada
 	 * @param parametro
 	 * @return
 	 * @throws IOException
 	 */
-	private BufferedImage getCorrectImageFor(
-			String tipoRecurso, String value) throws IOException {
+	private BufferedImage getCorrectImageFor(String tipoRecurso, String value)
+			throws IOException {
 		BufferedImage image;
-		if ("img".equals(tipoRecurso)) {			
-			image = BufferedImageGenerator
-					.generateBufferedImageFor(
-							ImageIO
-									.read(HTMLBoletoServlet.class
-											.getResource("/br/com/caelum/stella/boleto/img/"
-													+ value)),
-							BufferedImage.TYPE_INT_ARGB);
-		} else {			
-			//ainda tem que ajeitar a altura e talvez o tamanho da imagem gerada. Pode receber como parametro tb...
+		if ("img".equals(tipoRecurso)) {
+			image = ImageIO.read(HTMLBoletoServlet.class
+					.getResource("/br/com/caelum/stella/boleto/img/" + value));
+		} else {
+			// ainda tem que ajeitar a altura e talvez o tamanho da imagem
+			// gerada. Pode receber como parametro tb...
 			image = BufferedImageGenerator.generateBufferedImageFor(
-					BarcodeGenerator.generateBarcodeFor(value,37.00f),
+					BarcodeGenerator.generateBarcodeFor(value, 37.00f),
 					BufferedImage.TYPE_INT_ARGB);
 		}
 		return image;
