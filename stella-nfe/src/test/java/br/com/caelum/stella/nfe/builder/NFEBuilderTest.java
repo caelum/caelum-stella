6d37cdb83355d7bd4ec237783830ed343fbd7b06
Jonas Abreu <jonas@vidageek.net>
2009-07-13 17:36:27 -0300

    adding new test

---
+++ stella-nfe/src/test/java/br/com/caelum/stella/nfe/builder/NFEBuilderTest.java
@@ -0,0 +1,19 @@
+package br.com.caelum.stella.nfe.builder;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author jonasabreu
+ * 
+ */
+final public class NFEBuilderTest {
+
+    // introspector para verificar todas as classes se o método build está
+    // devolvendo this.
+
+    @Test
+    public void testThatAllBuilderReturnsParentOnBuildInvocation() {
+
+    }
+}
