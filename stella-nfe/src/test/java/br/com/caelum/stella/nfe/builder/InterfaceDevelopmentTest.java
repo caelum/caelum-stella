a245b1973cbf20b84100ab9a662da125ca37c11f
Jonas Abreu <jonas@vidageek.net>
2009-07-27 18:32:02 -0300

    moving converters to main source folder

--- stella-nfe/src/test/java/br/com/caelum/stella/nfe/builder/InterfaceDevelopmentTest.java
+++ stella-nfe/src/test/java/br/com/caelum/stella/nfe/builder/InterfaceDevelopmentTest.java
@@ -1,10 +1,16 @@
 package br.com.caelum.stella.nfe.builder;
 
+import org.junit.Test;
 
 /**
  * @author jonasabreu
  * 
  */
 final public class InterfaceDevelopmentTest {
+
+    @Test
+    public void test() {
+
+    }
 
 }
