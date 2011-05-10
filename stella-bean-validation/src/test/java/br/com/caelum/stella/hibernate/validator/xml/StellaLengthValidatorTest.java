9b052de9e6b78ed43e57678ed40d8cd0e8f4346a
davidpaniz <davidpaniz@gmail.com>
2011-05-10 16:21:10 -0300

    fixando testes

--- stella-bean-validation/src/test/java/br/com/caelum/stella/hibernate/validator/xml/StellaLengthValidatorTest.java
+++ stella-bean-validation/src/test/java/br/com/caelum/stella/hibernate/validator/xml/StellaLengthValidatorTest.java
@@ -1,75 +1,72 @@
 package br.com.caelum.stella.hibernate.validator.xml;
 
+import javax.validation.ConstraintValidatorContext;
+
 import junit.framework.Assert;
-import net.vidageek.mirror.Mirror;
+import net.vidageek.mirror.dsl.Mirror;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import br.com.caelum.stella.hibernate.validator.xml.logic.StellaLengthValidator;
 
 public class StellaLengthValidatorTest {
+	private ConstraintValidatorContext context = Mockito.mock(ConstraintValidatorContext.class);
 
-    @Test
-    public void testThatNullIsValid() {
-        StellaLengthValidator validator = new StellaLengthValidator();
-        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Length.class).atField("foo"));
-        Assert.assertTrue(validator.isValid(null));
-    }
+	@Test
+	public void testThatNullIsValid() {
+		StellaLengthValidator validator = new StellaLengthValidator();
+		validator.initialize(new Mirror().on(AnnotatedModel.class).reflect().annotation(Length.class).atField("foo"));
+		Assert.assertTrue(validator.isValid(null, context));
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testThatThrowsExceptionIfTypeIsNotString() {
-        StellaLengthValidator validator = new StellaLengthValidator();
-        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Length.class).atField("bar"));
-        Assert.assertTrue(validator.isValid(1));
-    }
+	@Test
+	public void testThatIsInvalidIfStringIsSmallerThanParameter() {
+		StellaLengthValidator validator = new StellaLengthValidator();
+		validator.initialize(new Mirror().on(AnnotatedModel.class).reflect().annotation(Length.class).atField("s1"));
+		Assert.assertFalse(validator.isValid("as", context));
+	}
 
-    @Test
-    public void testThatIsInvalidIfStringIsSmallerThanParameter() {
-        StellaLengthValidator validator = new StellaLengthValidator();
-        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Length.class).atField("s1"));
-        Assert.assertFalse(validator.isValid("as"));
-    }
+	@Test
+	public void testThatIsInvalidIfStringIsBiggerThanParameter() {
+		StellaLengthValidator validator = new StellaLengthValidator();
+		validator.initialize(new Mirror().on(AnnotatedModel.class).reflect().annotation(Length.class).atField("s2"));
+		Assert.assertFalse(validator.isValid("12345678912", context));
+	}
 
-    @Test
-    public void testThatIsInvalidIfStringIsBiggerThanParameter() {
-        StellaLengthValidator validator = new StellaLengthValidator();
-        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Length.class).atField("s2"));
-        Assert.assertFalse(validator.isValid("12345678912"));
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testThatThrowsExceptionIfMinIsBiggerThanMax() {
+		StellaLengthValidator validator = new StellaLengthValidator();
+		validator.initialize(new Mirror().on(AnnotatedModel.class).reflect().annotation(Length.class).atField("s4"));
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testThatThrowsExceptionIfMinIsBiggerThanMax() {
-        StellaLengthValidator validator = new StellaLengthValidator();
-        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Length.class).atField("s4"));
-    }
+	@Test(expected = IllegalStateException.class)
+	public void testThatThrowsExceptionIfMinIsNegative() {
+		StellaLengthValidator validator = new StellaLengthValidator();
+		validator.initialize(new Mirror().on(AnnotatedModel.class).reflect().annotation(Length.class).atField("s5"));
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testThatThrowsExceptionIfMinIsNegative() {
-        StellaLengthValidator validator = new StellaLengthValidator();
-        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Length.class).atField("s5"));
-    }
+	@Test
+	public void testThatIsValidIfBetweenMaxAndMin() {
+		StellaLengthValidator validator = new StellaLengthValidator();
+		validator.initialize(new Mirror().on(AnnotatedModel.class).reflect().annotation(Length.class).atField("s3"));
+		Assert.assertTrue(validator.isValid("1234567890", context));
+	}
 
-    @Test
-    public void testThatIsValidIfBetweenMaxAndMin() {
-        StellaLengthValidator validator = new StellaLengthValidator();
-        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Length.class).atField("s3"));
-        Assert.assertTrue(validator.isValid("1234567890"));
-    }
-
-    public static class AnnotatedModel {
-        @Length
-        public String foo = null;
-        @Length
-        public Integer bar = null;
-        @Length(min = 10)
-        public String s1;
-        @Length(max = 10)
-        public String s2;
-        @Length(min = 10, max = 10)
-        public String s3;
-        @Length(min = 11, max = 10)
-        public String s4;
-        @Length(min = -1, max = 10)
-        public String s5;
-    }
+	public static class AnnotatedModel {
+		@Length
+		public String foo = null;
+		@Length
+		public Integer bar = null;
+		@Length(min = 10)
+		public String s1;
+		@Length(max = 10)
+		public String s2;
+		@Length(min = 10, max = 10)
+		public String s3;
+		@Length(min = 11, max = 10)
+		public String s4;
+		@Length(min = -1, max = 10)
+		public String s5;
+	}
 }
