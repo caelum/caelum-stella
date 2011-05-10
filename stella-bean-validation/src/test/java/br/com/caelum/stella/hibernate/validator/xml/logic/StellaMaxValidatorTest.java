bd06285eaec22ad59559412f6cfc61c5b36a7f9a
Mario Amaral <mario.fts@gmail.com>
2011-05-10 16:36:21 -0300

    migrando testes

--- stella-bean-validation/src/test/java/br/com/caelum/stella/hibernate/validator/xml/logic/StellaMaxValidatorTest.java
+++ stella-bean-validation/src/test/java/br/com/caelum/stella/hibernate/validator/xml/logic/StellaMaxValidatorTest.java
@@ -1,24 +1,22 @@
 package br.com.caelum.stella.hibernate.validator.xml.logic;
 
+import javax.validation.ConstraintValidatorContext;
+
 import junit.framework.Assert;
 import net.vidageek.mirror.Mirror;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import br.com.caelum.stella.hibernate.validator.xml.Max;
 
 final public class StellaMaxValidatorTest {
+	private ConstraintValidatorContext context = Mockito.mock(ConstraintValidatorContext.class);
 
     @Test
     public void testThatNullIsValid() {
         StellaMaxValidator validator = new StellaMaxValidator();
-        Assert.assertTrue(validator.isValid(null));
-    }
-
-    @Test(expected = IllegalStateException.class)
-    public void testThatThrowsExceptionIfFieldIsNotAnIntegerType() {
-        StellaMaxValidator validator = new StellaMaxValidator();
-        Assert.assertTrue(validator.isValid(""));
+        Assert.assertTrue(validator.isValid(null,context));
     }
 
     @Test
@@ -26,10 +24,10 @@ final public class StellaMaxValidatorTest {
         StellaMaxValidator validator = new StellaMaxValidator();
 
         validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Max.class).atField("b"));
-        Assert.assertTrue(validator.isValid((byte) 10));
-        Assert.assertTrue(validator.isValid((short) 10));
-        Assert.assertTrue(validator.isValid(10));
-        Assert.assertTrue(validator.isValid((long) 10));
+        Assert.assertTrue(validator.isValid((byte) 10,context));
+        Assert.assertTrue(validator.isValid((short) 10,context));
+        Assert.assertTrue(validator.isValid(10,context));
+        Assert.assertTrue(validator.isValid((long) 10,context));
     }
 
     @Test
@@ -37,7 +35,7 @@ final public class StellaMaxValidatorTest {
         StellaMaxValidator validator = new StellaMaxValidator();
         validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Max.class).atField("b"));
 
-        Assert.assertFalse(validator.isValid(21));
+        Assert.assertFalse(validator.isValid(21,context));
     }
 
     @Test
@@ -45,7 +43,7 @@ final public class StellaMaxValidatorTest {
         StellaMaxValidator validator = new StellaMaxValidator();
         validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Max.class).atField("b"));
 
-        Assert.assertTrue(validator.isValid(10));
+        Assert.assertTrue(validator.isValid(10,context));
     }
 
     public static class AnnotatedModel {
