c0570e0289493cf5f8c39b3505045ea9687967a2
davidpaniz <davidpaniz@gmail.com>
2011-05-10 18:04:45 -0300

    adicionando transação ao teste do hibernate

--- stella-hibernate-user-types/src/test/java/br/com/caelum/stella/usertype/CpfUserTypeTest.java
+++ stella-hibernate-user-types/src/test/java/br/com/caelum/stella/usertype/CpfUserTypeTest.java
@@ -3,6 +3,7 @@ package br.com.caelum.stella.usertype;
 import junit.framework.Assert;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.classic.Session;
 import org.junit.BeforeClass;
@@ -25,11 +26,14 @@ public class CpfUserTypeTest {
 	@Test
 	public void shouldSaveAndLoadEntityMappedWithCpfUserType(){
 		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
 		PessoaFisica pessoa = new PessoaFisica();
 		CPF cpf = new CPF("555.555.555-55");
 		pessoa.setCpf(cpf);
 		session.save(pessoa);
+		transaction.commit();
 		session.flush();
+		session.close();
 		session = factory.openSession();
 		Long id = pessoa.getId();
 		PessoaFisica load = (PessoaFisica) session.load(PessoaFisica.class, id);
