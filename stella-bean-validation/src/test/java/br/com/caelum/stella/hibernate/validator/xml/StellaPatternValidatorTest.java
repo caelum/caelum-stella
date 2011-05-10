a4f25325a3c6a4387e110c1f20d588fe6596c001
Mario Amaral <mario.fts@gmail.com>
2011-05-10 16:34:33 -0300

    migrando testes

--- stella-bean-validation/src/test/java/br/com/caelum/stella/hibernate/validator/xml/StellaPatternValidatorTest.java
+++ stella-bean-validation/src/test/java/br/com/caelum/stella/hibernate/validator/xml/StellaPatternValidatorTest.java
@@ -1,55 +1,56 @@
 package br.com.caelum.stella.hibernate.validator.xml;
 
+import javax.validation.ConstraintValidatorContext;
+
 import junit.framework.Assert;
-import net.vidageek.mirror.Mirror;
+import net.vidageek.mirror.dsl.Mirror;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import br.com.caelum.stella.hibernate.validator.xml.logic.StellaPatternValidator;
 
 public class StellaPatternValidatorTest {
+	private ConstraintValidatorContext context = Mockito
+			.mock(ConstraintValidatorContext.class);
 
-    public static class AnnotatedModel {
-        @Pattern("")
-        public Integer i;
-    }
+	public static class AnnotatedModel {
+		@Pattern("")
+		public Integer i;
+	}
 
-    @Test(expected = IllegalStateException.class)
-    public void testThatThrowsExceptionIfFieldIsNotAString() {
-        StellaPatternValidator validator = new StellaPatternValidator();
-        validator.initialize(Mirror.on(AnnotatedModel.class).reflect().annotation(Pattern.class).atField("i"));
-        validator.isValid(new Integer(2));
-    }
+	public static class AnnotatedModel2 {
+		@Pattern("[ab]*c")
+		public String i;
+	}
 
-    public static class AnnotatedModel2 {
-        @Pattern("[ab]*c")
-        public String i;
-    }
+	@Test
+	public void testThatIsValidIfPatternMatchesWholeString() {
+		StellaPatternValidator validator = new StellaPatternValidator();
+		validator.initialize(new Mirror().on(AnnotatedModel2.class).reflect()
+				.annotation(Pattern.class).atField("i"));
+		Assert.assertEquals(true, validator.isValid("babac", context));
+	}
 
-    @Test
-    public void testThatIsValidIfPatternMatchesWholeString() {
-        StellaPatternValidator validator = new StellaPatternValidator();
-        validator.initialize(Mirror.on(AnnotatedModel2.class).reflect().annotation(Pattern.class).atField("i"));
-        Assert.assertEquals(true, validator.isValid("babac"));
-    }
+	public static class AnnotatedModel3 {
+		@Pattern("[ab]*")
+		public String i;
+	}
 
-    public static class AnnotatedModel3 {
-        @Pattern("[ab]*")
-        public String i;
-    }
+	@Test
+	public void testThatIsNotValidIfMatchIsPartial() {
+		StellaPatternValidator validator = new StellaPatternValidator();
+		validator.initialize(new Mirror().on(AnnotatedModel3.class).reflect()
+				.annotation(Pattern.class).atField("i"));
+		Assert.assertEquals(false, validator.isValid("babac", context));
+	}
 
-    @Test
-    public void testThatIsNotValidIfMatchIsPartial() {
-        StellaPatternValidator validator = new StellaPatternValidator();
-        validator.initialize(Mirror.on(AnnotatedModel3.class).reflect().annotation(Pattern.class).atField("i"));
-        Assert.assertEquals(false, validator.isValid("babac"));
-    }
-
-    @Test
-    public void testThatNullIsValid() {
-        StellaPatternValidator validator = new StellaPatternValidator();
-        validator.initialize(Mirror.on(AnnotatedModel3.class).reflect().annotation(Pattern.class).atField("i"));
-        Assert.assertEquals(true, validator.isValid(null));
-    }
+	@Test
+	public void testThatNullIsValid() {
+		StellaPatternValidator validator = new StellaPatternValidator();
+		validator.initialize(new Mirror().on(AnnotatedModel3.class).reflect()
+				.annotation(Pattern.class).atField("i"));
+		Assert.assertEquals(true, validator.isValid(null, context));
+	}
 
 }
--- stella-bean-validation/src/test/java/br/com/caelum/stella/hibernate/validator/xml/StellaRequiredValidatorTest.java
+++ stella-bean-validation/src/test/java/br/com/caelum/stella/hibernate/validator/xml/StellaRequiredValidatorTest.java
@@ -1,22 +1,26 @@
 package br.com.caelum.stella.hibernate.validator.xml;
 
+import javax.validation.ConstraintValidatorContext;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import br.com.caelum.stella.hibernate.validator.xml.logic.StellaRequiredValidator;
 
 public class StellaRequiredValidatorTest {
+	private ConstraintValidatorContext context = Mockito.mock(ConstraintValidatorContext.class);
 
     @Test
     public void testThatNullIsInvalid() {
         StellaRequiredValidator validator = new StellaRequiredValidator();
-        Assert.assertFalse(validator.isValid(null));
+        Assert.assertFalse(validator.isValid(null,context));
     }
 
     @Test
     public void testThatAnyReferenceIsValid() {
         StellaRequiredValidator validator = new StellaRequiredValidator();
-        Assert.assertTrue(validator.isValid(new Object()));
+        Assert.assertTrue(validator.isValid(new Object(),context));
     }
 }
