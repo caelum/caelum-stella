ef8157ceb877bc796b56fa61504eb7af6536cb8b
Paulo Silveira <paulo.silveira@caelum.com.br>
2011-05-18 01:52:04 -0300

    tentativa de reformulacao dos validadores de IE

--- stella-core/src/main/java/br/com/caelum/stella/validation/ie/IEConstraints.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/ie/IEConstraints.java
@@ -5,6 +5,7 @@ import br.com.caelum.stella.format.LeftSideZerosFormatter;
 import br.com.caelum.stella.validation.DigitoVerificadorInfo;
 import br.com.caelum.stella.validation.RotinaDeDigitoVerificador;
 import br.com.caelum.stella.validation.RotinaParameters;
+import br.com.caelum.stella.validation.ValidadorDeDV;
 
 /**
  * Documentação que pode ser consultada: <a
@@ -113,4 +114,28 @@ public class IEConstraints {
 
     }
 
+    static public class Acre {
+    		private static final int MOD = 11;
+
+    	    private static final RotinaDeDigitoVerificador[] rotinas = { IEConstraints.Rotina.E, IEConstraints.Rotina.POS_IE };
+
+    	    private static final int DVX_POSITION = 1 + 13;
+
+    	    private static final Integer[] DVX_MULTIPLIERS = IEConstraints.P1;
+
+    	    private static final DigitoVerificadorInfo DVX_INFO = new DigitoVerificadorInfo(0, rotinas, MOD, DVX_MULTIPLIERS,
+    	            DVX_POSITION);
+
+
+    	    private static final int DVY_POSITION = 1 + 12;
+
+    	    private static final Integer[] DVY_MULTIPLIERS = IEConstraints.P2;
+
+    	    private static final DigitoVerificadorInfo DVY_INFO = new DigitoVerificadorInfo(0, rotinas, MOD, DVY_MULTIPLIERS,
+    	            DVY_POSITION);
+
+    	    public static final ValidadorDeDV DVX_CHECKER = new ValidadorDeDV(DVX_INFO);
+
+    	    public static final ValidadorDeDV DVY_CHECKER = new ValidadorDeDV(DVY_INFO);
+    }
 }
