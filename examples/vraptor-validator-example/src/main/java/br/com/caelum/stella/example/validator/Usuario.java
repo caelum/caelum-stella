bcc9199693ec4467dd9cc36fb464ee5e424e819c
Mario Amaral <mario.fts@gmail.com>
2011-05-19 14:54:59 -0300

    refatorando exemplo do vraptor

--- examples/vraptor-validator-example/src/main/java/br/com/caelum/stella/example/validator/Usuario.java
+++ examples/vraptor-validator-example/src/main/java/br/com/caelum/stella/example/validator/Usuario.java
@@ -1,40 +1,17 @@
 package br.com.caelum.stella.example.validator;
 
-import br.com.caelum.stella.hibernate.validator.CNPJ;
 import br.com.caelum.stella.hibernate.validator.CPF;
-import br.com.caelum.stella.hibernate.validator.NIT;
 
 public class Usuario {
     @CPF
     private String cpf;
-    @CNPJ
-    private String cnpj;
 
-    public String getCpf() {
-        return cpf;
-    }
+	public String getCpf() {
+		return cpf;
+	}
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    @NIT
-    private String nit;
-
-    public String getNit() {
-        return nit;
-    }
-
-    public void setNit(String nit) {
-        this.nit = nit;
-    }
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
 }
