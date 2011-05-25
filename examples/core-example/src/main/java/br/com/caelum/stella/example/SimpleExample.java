64b3f968f8cae88ba0b4788e4285f3bc848424ef
davidpaniz <davidpaniz@gmail.com>
2011-05-25 18:04:28 -0300

    alterando site

--- examples/core-example/src/main/java/br/com/caelum/stella/example/SimpleExample.java
+++ examples/core-example/src/main/java/br/com/caelum/stella/example/SimpleExample.java
@@ -1,16 +1,18 @@
 package br.com.caelum.stella.example;
 
+import java.util.List;
+
 import br.com.caelum.stella.ValidationMessage;
 import br.com.caelum.stella.validation.CPFValidator;
 
 public class SimpleExample {
-    public static void main(String[] args) {
-        String cpf = "867.554.707-24";
-        for (ValidationMessage error : new CPFValidator().invalidMessagesFor(cpf)) {
-            System.out.println(error.getMessage());
-        }
+	public static void main(String[] args) {
+		String cpf = "867.554.707-24";
+		List<ValidationMessage> messages = new CPFValidator().invalidMessagesFor(cpf);
+		for (ValidationMessage error : messages) {
+			System.out.println(error.getMessage());
+		}
 
-        new CPFValidator().assertValid("867.554.707-24");
-
-    }
+		new CPFValidator().assertValid("867.554.707-24");
+	}
 }
