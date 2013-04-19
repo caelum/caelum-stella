18d799ea05941dc7b1cb3818a61905f7bab90ea9
Mario Amaral <mario.fts@gmail.com>
2013-04-19 20:15:44 -0300

    atualizando IE Amazonas

--- stella-core/src/test/java/br/com/caelum/stella/validation/ie/IEAmazonasValidatorTest.java
+++ stella-core/src/test/java/br/com/caelum/stella/validation/ie/IEAmazonasValidatorTest.java
@@ -17,7 +17,7 @@ public class IEAmazonasValidatorTest extends IEValidatorTest {
 	private static final String validString = "04.193.980-8";
 
 	private static final String[] validValues = { "04.345.678-2",
-			"04.193.980-8", "06.200.021-7",	"07.000.507-9" };
+			"04.193.980-8", "06.200.021-7",	"07.000.507-9", "04.104.862-8" };
 
 	@Override
 	protected Validator<String> getValidator(MessageProducer messageProducer,
