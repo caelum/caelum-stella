06ef2cab2a89bc20a708325d339aa8f22451869b
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-04-25 15:29:53 -0300

    atualizando IE Pernambuco #43

--- stella-core/src/main/java/br/com/caelum/stella/validation/ie/IEPernambucoValidator.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/ie/IEPernambucoValidator.java
@@ -1,63 +1,95 @@
 package br.com.caelum.stella.validation.ie;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import br.com.caelum.stella.MessageProducer;
 import br.com.caelum.stella.SimpleMessageProducer;
-import br.com.caelum.stella.ValidationMessage;
-import br.com.caelum.stella.validation.LogicOrComposedValidator;
-import br.com.caelum.stella.validation.Validator;
-import br.com.caelum.stella.validation.error.IEError;
+import br.com.caelum.stella.validation.DigitoVerificadorInfo;
+import br.com.caelum.stella.validation.RotinaDeDigitoVerificador;
+import br.com.caelum.stella.validation.ValidadorDeDV;
 
-public class IEPernambucoValidator implements Validator<String> {
+public class IEPernambucoValidator extends AbstractIEValidator {
 
-    private final LogicOrComposedValidator<String> baseValidator;
+    private static final int DVX_MOD = 11;
 
+    private static final int DVY_MOD = 11;
+
+    private static final String MISSING_ZEROS = "00000";
+
+    private static final int DVX_POSITION = MISSING_ZEROS.length() + 8;
+
+    private static final int DVY_POSITION = MISSING_ZEROS.length() + 9;
+
+    private static final Integer[] DVX_MULTIPLIERS = IEConstraints.P2;
+
+    private static final Integer[] DVY_MULTIPLIERS = IEConstraints.P13;
+
+    private static final RotinaDeDigitoVerificador[] DVX_ROTINAS = { IEConstraints.Rotina.E,
+            IEConstraints.Rotina.POS_IE };
+
+    private static final RotinaDeDigitoVerificador[] DVY_ROTINAS = { IEConstraints.Rotina.E,
+            IEConstraints.Rotina.POS_IE };
+
+    private static final DigitoVerificadorInfo DVX_INFO = new DigitoVerificadorInfo(0, DVX_ROTINAS, DVX_MOD,
+            DVX_MULTIPLIERS, DVX_POSITION);
+
+    private static final DigitoVerificadorInfo DVY_INFO = new DigitoVerificadorInfo(0, DVY_ROTINAS, DVY_MOD,
+            DVY_MULTIPLIERS, DVY_POSITION);
+
+    private static final ValidadorDeDV DVX_CHECKER = new ValidadorDeDV(DVX_INFO);
+
+    private static final ValidadorDeDV DVY_CHECKER = new ValidadorDeDV(DVY_INFO);
+
+    public static final Pattern FORMATED = Pattern.compile("\\d{7}-\\d{2}");
+
+    public static final Pattern UNFORMATED = Pattern.compile("(\\d{9})");
+
+    private static final String REPLACEMENT = MISSING_ZEROS + "$1";
+
+	
     /**
-     * Este considera, por padrão, que as cadeias estão formatadas e utiliza um
-     * {@linkplain SimpleMessageProducer} para geração de mensagens.
-     */
-    public IEPernambucoValidator() {
-        this(true);
-    }
+	 * Este considera, por padrão, que as cadeias estão formatadas e utiliza um
+	 * {@linkplain SimpleMessageProducer} para geração de mensagens.
+	 */
+	public IEPernambucoValidator() {
+		super(true);
+	}
 
-    /**
-     * O validador utiliza um {@linkplain SimpleMessageProducer} para geração de
-     * mensagens.
-     * 
-     * @param isFormatted
-     *            considerar cadeia formatada quando <code>true</code>
-     */
-    public IEPernambucoValidator(boolean isFormatted) {
-        this(new SimpleMessageProducer(), isFormatted);
-    }
+	/**
+	 * O validador utiliza um {@linkplain SimpleMessageProducer} para geração de
+	 * mensagens.
+	 * 
+	 * @param isFormatted
+	 *            considerar cadeia formatada quando <code>true</code>
+	 */
+	public IEPernambucoValidator(boolean isFormatted) {
+		super(isFormatted);
+	}
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
-    public IEPernambucoValidator(MessageProducer messageProducer, boolean isFormatted) {
-		Class[] validatorClasses = { IEPernambucoNovaValidator.class, IEPernambucoAntigaValidator.class };
-        this.baseValidator = new LogicOrComposedValidator<String>(messageProducer, isFormatted, validatorClasses);
-        this.baseValidator.setInvalidFormat(IEError.INVALID_FORMAT);
-    }
+	public IEPernambucoValidator(MessageProducer messageProducer, boolean isFormatted) {
+		super(messageProducer, isFormatted);
+	}
 
-    public void assertValid(String value) {
-        if (value != null) {
-            baseValidator.assertValid(value);
+
+	@Override
+	protected Pattern getUnformattedPattern() {
+		return UNFORMATED;
+	}
+
+	@Override
+	protected Pattern getFormattedPattern() {
+		return FORMATED;
+	}
+
+    protected boolean hasValidCheckDigits(String value) {
+        String testedValue = null;
+        Matcher matcher = UNFORMATED.matcher(value);
+        if (matcher.matches()) {
+            testedValue = matcher.replaceAll(REPLACEMENT);
         }
+        boolean DVXisValid = DVX_CHECKER.isDVValid(testedValue);
+        boolean DVYisValid = DVY_CHECKER.isDVValid(testedValue);
+        return (DVXisValid) && (DVYisValid);
     }
-
-    public List<ValidationMessage> invalidMessagesFor(String value) {
-        List<ValidationMessage> result;
-        if (value != null) {
-            result = baseValidator.invalidMessagesFor(value);
-        } else {
-            result = new ArrayList<ValidationMessage>();
-        }
-        return result;
-    }
-
-    public boolean isEligible(String object) {
-        return baseValidator.isEligible(object);
-    }
-
 }
