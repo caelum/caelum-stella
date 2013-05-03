54f630550c76f4e20d644761c6f01c059689ee7b
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-05-03 13:42:31 -0300

    Alguns testes p/ geração de boleto Caixa

--- stella-boleto/src/test/java/br/com/caelum/stella/boleto/bancos/CaixaTest.java
+++ stella-boleto/src/test/java/br/com/caelum/stella/boleto/bancos/CaixaTest.java
@@ -16,9 +16,14 @@ public class CaixaTest {
 	private Boleto boleto;
 	private Caixa banco = new Caixa();
 	private Emissor emissor;
+	private Boleto outroBoleto;
 
 	@Before
 	public void setUp() {
+
+		///////////////////////////////////////////////////////////////////
+		// SET UP de boleto com carteira SINCO
+		///////////////////////////////////////////////////////////////////
 
 		Datas datas = Datas.newDatas().withDocumento(22, 04, 2013)
 			.withProcessamento(22, 04, 2013).withVencimento(29, 04, 2013);
@@ -33,6 +38,25 @@ public class CaixaTest {
 		this.boleto = Boleto.newBoleto().withDatas(datas)
 			.withEmissor(emissor).withBanco(this.banco).withSacado(sacado)
 			.withValorBoleto(4016.10).withNumeroDoDocumento("3084373");
+		
+		///////////////////////////////////////////////////////////////////
+		// SET UP de boleto com carteira Sem Registro e Registrada no SIGCB
+		///////////////////////////////////////////////////////////////////
+		
+		Datas datasOutroBoleto = Datas.newDatas().withDocumento(21, 8, 2012)
+				.withProcessamento(21, 8, 2012).withVencimento(04, 9, 2012);
+
+		Emissor emissor = Emissor.newEmissor().withCedente("Guilherme")
+			.withAgencia(589).withCarteira(2)
+			.withContaCorrente(290274).withDigitoContaCorrente('5')
+			.withNossoNumero(24900000000000132l)
+			.withDigitoNossoNumero("3");
+
+		Sacado sacadoOutroBoleto = Sacado.newSacado().withNome("Paulo");
+
+		this.outroBoleto = Boleto.newBoleto().withDatas(datasOutroBoleto)
+			.withEmissor(emissor).withBanco(banco).withSacado(sacadoOutroBoleto)
+			.withValorBoleto(80.00).withNumeroDoDocumento("00000000132");
 	}
 
 	@Test
@@ -71,7 +95,7 @@ public class CaixaTest {
 	}
 
 	@Test
-	public void testLinhaDoBancoCaixa() {
+	public void testLinhaDoBancoCaixaComCarteiraSINCO() {
 		GeradorDeLinhaDigitavel gerador = new GeradorDeLinhaDigitavel();
 		String codigoDeBarras = boleto.getBanco().geraCodigoDeBarrasPara(this.boleto);
 		String linha = "10491.00231  59990.000008  00039.944582  2  56830000401610";
@@ -79,11 +103,32 @@ public class CaixaTest {
 	}
 
 	@Test
-	public void testCodigoDeBarraDoBancoCaixa() {
+	public void testCodigoDeBarraDoBancoCaixaComCarteiraSINCO() {
 		assertEquals("10492568300004016101002359990000000003994458",
 				this.banco.geraCodigoDeBarrasPara(this.boleto));
 	}
 
+	@Test
+	public void testLinhaDoBancoCaixaComCarteiraSICGB() {
+		GeradorDeLinhaDigitavel gerador = new GeradorDeLinhaDigitavel();
+		String codigoDeBarras = outroBoleto.getBanco().geraCodigoDeBarrasPara(outroBoleto);
+		String linha = "10492.90271  45900.200044  00000.013227  9  54460000008000";
+		assertEquals(linha, gerador.geraLinhaDigitavelPara(codigoDeBarras));
+	}
+
+	@Test
+	public void testCodigoDeBarraDoBancoCaixaComCarteiraSICGB() {
+		assertEquals("10499544600000080002902745900200040000001322",
+				this.banco.geraCodigoDeBarrasPara(this.outroBoleto));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void deveLancarExceptionSeCarteiraNaoForSuportada() throws Exception {
+		this.outroBoleto.getEmissor().withCarteira(3);
+		assertEquals("10499544600000080002902745900200040000001322",
+				this.banco.geraCodigoDeBarrasPara(this.outroBoleto));
+	}
+	
 	@Test
 	public void testGetImage() {
 		assertNotNull(this.banco.getImage());
