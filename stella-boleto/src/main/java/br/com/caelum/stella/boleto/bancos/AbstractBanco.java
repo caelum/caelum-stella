ba8e962630ee87019bafd37125094a9365ceb17e
Rodrigo Turini <rodrigo.turini@caelum.com.br>
2013-06-14 12:28:17 -0300

    Corrigindo String Builder do AbstractBanco

--- stella-boleto/src/main/java/br/com/caelum/stella/boleto/bancos/AbstractBanco.java
+++ stella-boleto/src/main/java/br/com/caelum/stella/boleto/bancos/AbstractBanco.java
@@ -16,7 +16,8 @@ public abstract class AbstractBanco implements Banco {
 
 	@Override
 	public String getNossoNumeroECodDocumento(Emissor emissor) {
-		StringBuilder builder = new StringBuilder(emissor.getCarteira());
+		StringBuilder builder = new StringBuilder().append(emissor.getCarteira());
+		System.out.println("carteira" + emissor.getCarteira());
 		builder.append("/").append(getNossoNumeroDoEmissorFormatado(emissor));
 		return builder.toString();
 	}
