f5fe573e4e67bd78a2c5a61eaf2331a0e270a246
Mario Amaral <mario.fts@gmail.com>
2014-01-14 20:40:22 -0200

    simplificando a formatação de campos

--- stella-boleto/src/main/java/br/com/caelum/stella/boleto/transformer/FormatadorDeBoleto.java
+++ stella-boleto/src/main/java/br/com/caelum/stella/boleto/transformer/FormatadorDeBoleto.java
@@ -1,31 +1,15 @@
 package br.com.caelum.stella.boleto.transformer;
 
-import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 class FormatadorDeBoleto {
-
-	private static NumberFormat nfmt = NumberFormat
-			.getNumberInstance(new Locale("pt", "BR"));
-	
-	private static SimpleDateFormat dfmt = new SimpleDateFormat("dd/MM/yyyy"); 
-	
-	static {
-		nfmt.setMinimumFractionDigits(2);
-		nfmt.setMaximumFractionDigits(2);
-	}
-	
 	static String formataData(final Calendar data) {
-		synchronized (dfmt) {
-			return dfmt.format(data.getTime());
-		}
+			return new SimpleDateFormat("dd/MM/yyyy").format(data.getTime());
 	}
 
 	static String formataValor(final double valor) {
-		synchronized (nfmt) {
-			return nfmt.format(valor);
-		}
+		return String.format(new Locale("pt", "BR"),"%10.2f", valor);
 	}
 }
