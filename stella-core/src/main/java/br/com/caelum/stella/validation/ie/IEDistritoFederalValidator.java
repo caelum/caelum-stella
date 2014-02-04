f172131a737688c53fcf5f9c296df473cd15fa42
Deco Verri <decoverri@gmail.com>
2014-02-04 17:59:38 -0200

    Usando DigitoPara no IEDistritoFederalValidator

--- stella-core/src/main/java/br/com/caelum/stella/validation/ie/IEDistritoFederalValidator.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/ie/IEDistritoFederalValidator.java
@@ -2,11 +2,9 @@ package br.com.caelum.stella.validation.ie;
 
 import java.util.regex.Pattern;
 
+import br.com.caelum.stella.DigitoPara;
 import br.com.caelum.stella.MessageProducer;
 import br.com.caelum.stella.SimpleMessageProducer;
-import br.com.caelum.stella.validation.DigitoVerificadorInfo;
-import br.com.caelum.stella.validation.RotinaDeDigitoVerificador;
-import br.com.caelum.stella.validation.ValidadorDeDV;
 
 /**
  * <p>
@@ -20,28 +18,7 @@ import br.com.caelum.stella.validation.ValidadorDeDV;
  */
 public class IEDistritoFederalValidator extends AbstractIEValidator {
 
-    private static final int MOD = 11;
-
-    private static final int DVX_POSITION = 1 + 12;
-
-    private static final RotinaDeDigitoVerificador[] rotinas = { IEConstraints.Rotina.E, IEConstraints.Rotina.POS_IE };
-
-    private static final Integer[] DVX_MULTIPLIERS = IEConstraints.P2;
-
-    private static final DigitoVerificadorInfo DVX_INFO = new DigitoVerificadorInfo(0, rotinas, MOD, DVX_MULTIPLIERS,
-            DVX_POSITION);
-
-    private static final ValidadorDeDV DVX_CHECKER = new ValidadorDeDV(DVX_INFO);
-
-    private static final Integer[] DVY_MULTIPLIERS = IEConstraints.P1;
-
-    private static final int DVY_POSITION = 1 + 13;
-
-    private static final DigitoVerificadorInfo DVY_INFO = new DigitoVerificadorInfo(0, rotinas, MOD, DVY_MULTIPLIERS,
-            DVY_POSITION);
-
-    private static final ValidadorDeDV DVY_CHECKER = new ValidadorDeDV(DVY_INFO);
-    /*
+	/*
      * Formato: 07.408.738/002-50
      */
 
@@ -83,9 +60,23 @@ public class IEDistritoFederalValidator extends AbstractIEValidator {
 		return FORMATED;
 	}
 	
-    protected boolean hasValidCheckDigits(String value) {
-        String testedValue = IEConstraints.PRE_VALIDATION_FORMATTER.format(value);
-        return DVX_CHECKER.isDVValid(testedValue) && DVY_CHECKER.isDVValid(testedValue);
-    }
+    protected boolean hasValidCheckDigits(String unformattedIE) {
+		String iESemDigito = unformattedIE.substring(0, unformattedIE.length() - 2);
+		String digitos = unformattedIE.substring(unformattedIE.length() - 2);
+		String digitosCalculados = calculaDigitos(iESemDigito);
+
+		return digitos.equals(digitosCalculados);
+	}
+
+	private String calculaDigitos(String iESemDigito) {
+		DigitoPara digitoPara = new DigitoPara(iESemDigito);
+		digitoPara.complementarAoModulo().trocandoPorSeEncontrar("0", 10, 11);
+
+		String digito1 = digitoPara.calcula();
+		digitoPara.addDigito(digito1);
+		String digito2 = digitoPara.calcula();
+
+		return digito1 + digito2;
+	}
 
 }
