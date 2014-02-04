d2305de29efe2b871f1a49a93b46535623d230e2
Deco Verri <decoverri@gmail.com>
2014-02-04 17:48:33 -0200

    Usando DigitoPara no IEBahiaValidator

--- stella-core/src/main/java/br/com/caelum/stella/validation/ie/IEBahiaValidator.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/ie/IEBahiaValidator.java
@@ -2,11 +2,9 @@ package br.com.caelum.stella.validation.ie;
 
 import java.util.regex.Pattern;
 
+import br.com.caelum.stella.DigitoPara;
 import br.com.caelum.stella.MessageProducer;
 import br.com.caelum.stella.SimpleMessageProducer;
-import br.com.caelum.stella.validation.DigitoVerificadorInfo;
-import br.com.caelum.stella.validation.RotinaDeDigitoVerificador;
-import br.com.caelum.stella.validation.ValidadorDeDV;
 
 /**
  * <p>
@@ -20,45 +18,14 @@ import br.com.caelum.stella.validation.ValidadorDeDV;
  */
 public class IEBahiaValidator extends AbstractIEValidator {
 
-    private static final RotinaDeDigitoVerificador[] rotinas = { IEConstraints.Rotina.E, IEConstraints.Rotina.POS_IE };
+	/*
+	 * 612345-57
+	 * 
+	 * 123456-63
+	 */
+	public static final Pattern FORMATED = Pattern.compile("(\\d{6,7}-\\d{2})|(\\d{2,3}\\.\\d{3}\\.\\d{3})");
 
-    private static final int DVX_POSITION = 6 + 8;
-
-    private static final int DVY_POSITION = 6 + 7;
-
-    private static final Integer[] DVX_MULTIPLIERS = IEConstraints.P2;
-
-    private static final Integer[] DVY_MULTIPLIERS = IEConstraints.P3;
-
-    private static final DigitoVerificadorInfo DVX_INFO_MOD11 = new DigitoVerificadorInfo(0, rotinas, 11,
-            DVX_MULTIPLIERS, DVX_POSITION);
-
-    private static final ValidadorDeDV DVX_CHECKER_MOD11 = new ValidadorDeDV(DVX_INFO_MOD11);
-
-    private static final DigitoVerificadorInfo DVY_INFO_MOD11 = new DigitoVerificadorInfo(0, rotinas, 11,
-            DVY_MULTIPLIERS, DVY_POSITION);
-
-    private static final ValidadorDeDV DVY_CHECKER_MOD11 = new ValidadorDeDV(DVY_INFO_MOD11);
-
-    private static final DigitoVerificadorInfo DVX_INFO_MOD10 = new DigitoVerificadorInfo(0, rotinas, 10,
-            DVX_MULTIPLIERS, DVX_POSITION);
-
-    private static final ValidadorDeDV DVX_CHECKER_MOD10 = new ValidadorDeDV(DVX_INFO_MOD10);
-
-    private static final DigitoVerificadorInfo DVY_INFO_MOD10 = new DigitoVerificadorInfo(0, rotinas, 10,
-            DVY_MULTIPLIERS, DVY_POSITION);
-
-    private static final ValidadorDeDV DVY_CHECKER_MOD10 = new ValidadorDeDV(DVY_INFO_MOD10);
-
-
-    /*
-     * 612345-57
-     * 
-     * 123456-63
-     */
-    public static final Pattern FORMATED = Pattern.compile("(\\d{6,7}-\\d{2})|(\\d{2,3}\\.\\d{3}\\.\\d{3})");
-
-    public static final Pattern UNFORMATED = Pattern.compile("\\d{8,9}");
+	public static final Pattern UNFORMATED = Pattern.compile("\\d{8,9}");
 
 	/**
 	 * Este considera, por padrão, que as cadeias estão formatadas e utiliza um
@@ -83,7 +50,6 @@ public class IEBahiaValidator extends AbstractIEValidator {
 		super(messageProducer, isFormatted);
 	}
 
-
 	@Override
 	protected Pattern getUnformattedPattern() {
 		return UNFORMATED;
@@ -93,29 +59,42 @@ public class IEBahiaValidator extends AbstractIEValidator {
 	protected Pattern getFormattedPattern() {
 		return FORMATED;
 	}
-  
-    protected boolean hasValidCheckDigits(String value) {
-        String testedValue = IEConstraints.PRE_VALIDATION_FORMATTER.format(value);
-        boolean result = false;
-        char charToCheck;
-        
-        if(value.length() == 8){
-        	charToCheck= value.charAt(0);
-        }else{
-        	charToCheck= value.charAt(1);
-        }
-        
-		switch (charToCheck) {
-        case '6':
-        case '7':
-        case '9':
-            result = DVX_CHECKER_MOD11.isDVValid(testedValue) && DVY_CHECKER_MOD11.isDVValid(testedValue);
-            break;
-        default:
-            result = DVX_CHECKER_MOD10.isDVValid(testedValue) && DVY_CHECKER_MOD10.isDVValid(testedValue);
-        }
-        return result;
-    }
 
+	protected boolean hasValidCheckDigits(String unformattedIE) {
+		String iESemDigito = unformattedIE.substring(0, unformattedIE.length() - 2);
+		String digitos = unformattedIE.substring(unformattedIE.length() - 2);
+
+		String digitosCalculados = calculaDigito(iESemDigito);
+
+		return digitos.equals(digitosCalculados);
+	}
+
+	private String calculaDigito(String iESemDigito) {
+		DigitoPara digitoPara = new DigitoPara(iESemDigito);
+		digitoPara.complementarAoModulo().trocandoPorSeEncontrar("0", 10, 11);
+
+		char charToCheck;
+		if (iESemDigito.length() == 6) {
+			charToCheck = iESemDigito.charAt(0);
+		} else {
+			charToCheck = iESemDigito.charAt(1);
+		}
+
+		switch (charToCheck) {
+		case '6':
+		case '7':
+		case '9':
+			digitoPara.mod(11);
+			break;
+		default:
+			digitoPara.mod(10);
+		}
+
+		String digito2 = digitoPara.calcula();
+		digitoPara.addDigito(digito2);
+		String digito1 = digitoPara.calcula();
+
+		return digito1 + digito2;
+	}
 
 }
