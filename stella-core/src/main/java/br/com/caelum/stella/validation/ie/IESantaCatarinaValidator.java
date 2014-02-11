194c4f470002a950658646d0ea81c810ecdedc48
Deco Verri <decoverri@gmail.com>
2014-02-11 17:57:25 -0200

    Usando DigitoPara no IESantaCatarinaValidator

--- stella-core/src/main/java/br/com/caelum/stella/validation/ie/IESantaCatarinaValidator.java
+++ stella-core/src/main/java/br/com/caelum/stella/validation/ie/IESantaCatarinaValidator.java
@@ -2,29 +2,11 @@ package br.com.caelum.stella.validation.ie;
 
 import java.util.regex.Pattern;
 
+import br.com.caelum.stella.DigitoPara;
 import br.com.caelum.stella.MessageProducer;
 import br.com.caelum.stella.SimpleMessageProducer;
-import br.com.caelum.stella.validation.DigitoVerificadorInfo;
-import br.com.caelum.stella.validation.RotinaDeDigitoVerificador;
-import br.com.caelum.stella.validation.ValidadorDeDV;
 
 public class IESantaCatarinaValidator extends AbstractIEValidator {
-
-    private static final int MOD = 11;
-
-    // TAMANHO = 9;
-    private static final String MISSING_LEFT_SIDE_ZEROS = "00000";
-
-    private static final int DVX_POSITION = MISSING_LEFT_SIDE_ZEROS.length() + 9;
-
-    private static final Integer[] DVX_MULTIPLIERS = IEConstraints.P1;
-
-    private static final RotinaDeDigitoVerificador[] rotinas = { IEConstraints.Rotina.E, IEConstraints.Rotina.POS_IE };
-
-    private static final DigitoVerificadorInfo DVX_INFO = new DigitoVerificadorInfo(0, rotinas, MOD, DVX_MULTIPLIERS,
-            DVX_POSITION);
-
-    private static final ValidadorDeDV DVX_CHECKER = new ValidadorDeDV(DVX_INFO);
 
     public static final Pattern FORMATED = Pattern.compile("\\d{3}(\\.?\\d{3}){2}");
 
@@ -64,9 +46,19 @@ public class IESantaCatarinaValidator extends AbstractIEValidator {
 		return FORMATED;
 	}
 	
-    protected boolean hasValidCheckDigits(String value) {
-        String testedValue = MISSING_LEFT_SIDE_ZEROS + value;
-        return (DVX_CHECKER.isDVValid(testedValue));
+    protected boolean hasValidCheckDigits(String unformattedIE) {
+		String iESemDigito = unformattedIE.substring(0, unformattedIE.length() - 1);
+		String digito = unformattedIE.substring(unformattedIE.length() - 1);
+
+		String digitoCalculado = calculaDigito(iESemDigito);
+
+		return digito.equals(digitoCalculado);
     }
 
+	private String calculaDigito(String iESemDigito) {
+		DigitoPara digitoPara = new DigitoPara(iESemDigito);
+		digitoPara.complementarAoModulo().trocandoPorSeEncontrar("0", 10, 11);
+
+		return digitoPara.calcula();
+	}
 }
