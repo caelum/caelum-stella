6ecac2acb64a04db9450d962b2cc981a64c1e822
Mario Amaral <mario.fts@gmail.com>
2014-02-27 13:42:23 -0300

    nova classe para fazer o leftpad das strings

---
+++ stella-boleto/src/test/java/br/com/caelum/stella/boleto/utils/StellaStringUtilsTest.java
@@ -0,0 +1,34 @@
+package br.com.caelum.stella.boleto.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StellaStringUtilsTest {
+
+	@Test
+	public void deveCompletarUmaStringComZerosAEsquerda() {
+		String value = "123";
+		String result = StellaStringUtils.lefPadWithZeros(value, 5);
+		Assert.assertEquals("00123", result);
+	}
+	
+	@Test
+	public void naoDeveApendarNadaSeOtamanhoJaEhCerto() {
+		String value = "12345";
+		String result = StellaStringUtils.lefPadWithZeros(value, 5);
+		Assert.assertEquals("12345", result);
+	}
+	
+	@Test
+	public void naoDeveApendarNadaSeOtamanhoEhMenorQueAString() {
+		String value = "12345";
+		String result = StellaStringUtils.lefPadWithZeros(value, 4);
+		Assert.assertEquals("12345", result);
+	}
+	
+	
+	
+
+}
