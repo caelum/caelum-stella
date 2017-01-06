da7d6c1fc180b2f592e8ead27842f35002566079
Gilberto Caetano de Andrade <gilbertoca@gmail.com>
2017-01-06 11:03:07 -0300

    Javadoc
    Descrevendo melhor o intuito da classe e coordenadas de utilização

--- stella-boleto/src/main/java/br/com/caelum/stella/boleto/bancos/BancoDoBrasil.java
+++ stella-boleto/src/main/java/br/com/caelum/stella/boleto/bancos/BancoDoBrasil.java
@@ -8,29 +8,39 @@ import static br.com.caelum.stella.boleto.utils.StellaStringUtils.leftPadWithZer
 /**
  * Gera dados de um boleto relativos ao Banco do Brasil.
  * 
- * O manual do BB(janeiro/2016) em relação ao código de barras foi o seguinte.
+ * O manual do BB(janeiro/2016) em relação ao código de barras relata o seguinte.
  * Leiaute com 2 campos (44 posições):
  *
  * 1. obrigatório, FEBRABAN com 19 posições;
  * 2. campo livre, determinado pela modalidade de cobrança utilizada pelo cliente com 25 posições.
  *
- * A classe BancoDoBrasil deveria atuar na composição do campo livre seguindo o item 2.3.2, que diz " Os padrões do BB estão identificados nos Anexos VII, VIII, IX e X".
+ * A classe BancoDoBrasil deve atuar na composição do campo livre seguindo o item 2.3.2, que diz " Os padrões do BB estão identificados nos Anexos VII, VIII, IX e X".
  * Cada anexo é regido pelo número do convênio e não pela carteira como se vê atualmente na classe.
  * O cliente recebe um número de convênio, podendo ser de 4, 6 ou 7 posições - e cada algoritmo de formação do campo livre leva isso em consideração.
+ * Nessa implementação a formação/composição do NOSSO NUMERO fica a cargo do desenvolvedor. O NOSSO NUMERO nada mais é
+ * do que a união dos campos CONVENIO e COMPLEMENTO, com pequenos requisitos de formatação.
+ * Para Boleto registrado:
  *
- * Boleto Registrado:
+ * 1. Anexo VII, convênio 4 posições [convênio + complemento + agência + conta + carteira] CCCCNNNNNNN
+ * 2. Anexo VIII, convênio 6 posições [convênio + complemento + agência + conta + carteira] CCCCCCNNNNN
+ * 3. Anexo IX, convênio 7 posições [000000 + convênio + complemento + carteira] CCCCCCCNNNNNNNNNN
  *
- * 1. Anexo VII, convênio 4 posições [convênio + complemento + agência + conta + carteira]
- * 2. Anexo VIII, convênio 6 posições [convênio + complemento + agência + conta + carteira]
- * 3. Anexo IX, convênio 7 posições [000000 + convênio + complemento + carteira]
+ * Finalmente, Boleto sem registro:
  *
- * Finalmente, Boleto Sem Registro:
+ * 4. Anexo X, convênio 6 posições [convênio + complemento + carteira] NNNNNNNNNNNNNNNNN
  *
- * 4. Anexo X, convênio 6 posições [convênio + complemento + carteira]
+ * Um exemplo para número do convênio [1234567]: 
+ * String numeroConvenio = "1234567";
+ * int numeroComplemento = 206; //identificador interno do BB ou do beneficiário
+ * //Responsabilidade do desenvolvedor compor o nosso número
+ * String nossoNumero = numeroConvenio + String.format("%010d", numeroComplemento);
  *
+ * Referência: www.bb.com.br/docs/pub/emp/mpe/espeboletobb.pdf
+ * Útimo acesso: 06/01/2017
+ *
+ * @author Gilberto C Andrade 
  * @author Cauê Guerra
  * @author Paulo Silveira
- * @author Gilberto C Andrade
  */
 public class BancoDoBrasil extends AbstractBanco implements Banco {
 
